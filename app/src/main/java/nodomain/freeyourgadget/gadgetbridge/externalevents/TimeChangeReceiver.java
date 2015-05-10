e78c912be35713306917162a8488195f1478924b
Andreas Shimokawa <shimokawa@fsfe.org>
2015-05-10 11:21:16 +0200

    Only sync time on time/timezone changes if appropriate option is set.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/TimeChangeReceiver.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/TimeChangeReceiver.java
@@ -3,6 +3,8 @@ package nodomain.freeyourgadget.gadgetbridge.externalevents;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import nodomain.freeyourgadget.gadgetbridge.BluetoothCommunicationService;
@@ -14,9 +16,10 @@ public class TimeChangeReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         final String action = intent.getAction();
 
-        if (action.equals(Intent.ACTION_TIME_CHANGED) || action.equals(Intent.ACTION_TIMEZONE_CHANGED)) {
+        if (sharedPrefs.getBoolean("datetime_synconconnect", true) && (action.equals(Intent.ACTION_TIME_CHANGED) || action.equals(Intent.ACTION_TIMEZONE_CHANGED))) {
             Log.i(TAG, "Time or Timezone changed, syncing with device");
             Intent startIntent = new Intent(context, BluetoothCommunicationService.class);
             startIntent.setAction(BluetoothCommunicationService.ACTION_SETTIME);
