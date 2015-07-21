3662f32dbfbc0a3212ef9598f6fc12c1eee9fc30
cpfeiffer <cpfeiffer@users.noreply.github.com>
2015-07-21 21:42:04 +0200

    Remove duplicated constants

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/miband/MiBandSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/miband/MiBandSupport.java
@@ -60,16 +60,6 @@ public class MiBandSupport extends AbstractBTLEDeviceSupport {
 
     private static final Logger LOG = LoggerFactory.getLogger(MiBandSupport.class);
 
-    public static final int MODE_CHARGING = 6;
-    public static final int MODE_NONWEAR = 3;
-    public static final int MODE_NREM = 5;
-    public static final int MODE_ONBED = 7;
-    public static final int MODE_REM = 4;
-    public static final int MODE_RUNNING = 2;
-    public static final int MODE_SLIENT = 0;
-    public static final int MODE_USER = 100;
-    public static final int MODE_WALKING = 1;
-
     //temporary buffer, size is a multiple of 60 because we want to store complete minutes (1 minute = 3 bytes)
     private static final int activityDataHolderSize = 3 * 60 * 4; // 8h
     private byte[] activityDataHolder = new byte[activityDataHolderSize];
