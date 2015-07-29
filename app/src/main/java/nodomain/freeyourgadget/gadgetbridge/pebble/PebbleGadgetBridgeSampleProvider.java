0eeb5a6479afb8888a356f40e1847676614c4e34
Andreas Shimokawa <shimokawa@fsfe.org>
2015-07-29 23:28:10 +0200

    PebbleGadgetBridgeSampleProvider: properly override getID()

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/pebble/PebbleGadgetBridgeSampleProvider.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/pebble/PebbleGadgetBridgeSampleProvider.java
@@ -1,7 +1,14 @@
 package nodomain.freeyourgadget.gadgetbridge.pebble;
 
+import nodomain.freeyourgadget.gadgetbridge.model.SampleProvider;
+
 public class PebbleGadgetBridgeSampleProvider extends MorpheuzSampleProvider {
     public PebbleGadgetBridgeSampleProvider() {
         movementDivisor = 63.0f;
+    }
+
+    @Override
+    public byte getID() {
+        return SampleProvider.PROVIDER_PEBBLE_GADGETBRIDGE;
     }
 }
