eb2332c8be0d70c78f0e0baede544b90963d8e9f
cpfeiffer <cpfeiffer@users.noreply.github.com>
2015-08-13 23:23:40 +0200

    Some preparations for unit tests

---
+++ app/src/test/java/nodomain/freeyourgadget/gadgetbridge/service/btle/BtLEQueueTest.java
@@ -0,0 +1,19 @@
+package nodomain.freeyourgadget.gadgetbridge.service.btle;
+
+import junit.framework.TestCase;
+
+public class BtLEQueueTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testIsConnected() throws Exception {
+
+    }
+}
