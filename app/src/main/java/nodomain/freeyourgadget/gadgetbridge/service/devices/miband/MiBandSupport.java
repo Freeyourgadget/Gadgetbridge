2e6536555b806225d7304fedbed730a01f5ff512
cpfeiffer <cpfeiffer@users.noreply.github.com>
2016-05-31 22:56:22 +0200

    Fix previous commit (compile!)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/miband/MiBandSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/miband/MiBandSupport.java
@@ -361,7 +361,7 @@ public class MiBandSupport extends AbstractBTLEDeviceSupport {
         LOG.info("Attempting to set wear location...");
         BluetoothGattCharacteristic characteristic = getCharacteristic(MiBandService.UUID_CHARACTERISTIC_CONTROL_POINT);
         if (characteristic != null) {
-            transaction.add(new ConditionalWriteAction() {
+            transaction.add(new ConditionalWriteAction(characteristic) {
                 @Override
                 protected byte[] checkCondition() {
                     if (getDeviceInfo() != null && getDeviceInfo().isAmazFit()) {
