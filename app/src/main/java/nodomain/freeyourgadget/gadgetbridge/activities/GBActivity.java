0fb664c1410b957cae028143eb152d72e35ec9f7
Andreas Shimokawa <shimokawa@fsfe.org>
2016-06-05 14:33:09 +0200

    allow to switch languages at runtime

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/GBActivity.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/GBActivity.java
@@ -1,14 +1,33 @@
 package nodomain.freeyourgadget.gadgetbridge.activities;
 
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Locale;
+
 import nodomain.freeyourgadget.gadgetbridge.GBApplication;
 import nodomain.freeyourgadget.gadgetbridge.R;
+import nodomain.freeyourgadget.gadgetbridge.util.Prefs;
 
 
 public class GBActivity extends AppCompatActivity {
+    private void setLanguage(String language) {
+        Locale locale;
+        if (language.equals("default")) {
+            locale = Locale.getDefault();
+        } else {
+            locale = new Locale(language);
+        }
+        Configuration config = new Configuration();
+        config.locale = locale;
+
+        // FIXME: I have no idea what I am doing
+        getApplicationContext().getResources().updateConfiguration(config, getApplicationContext().getResources().getDisplayMetrics());
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (GBApplication.isDarkThemeEnabled()) {
@@ -17,6 +36,9 @@ public class GBActivity extends AppCompatActivity {
             setTheme(R.style.GadgetbridgeTheme);
         }
 
+        Prefs prefs = GBApplication.getPrefs();
+        String language = prefs.getString("language", "default");
+        setLanguage(language);
         super.onCreate(savedInstanceState);
     }
 }
