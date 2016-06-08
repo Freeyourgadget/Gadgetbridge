1d5c8bae9d7a0070185fbcb1026a04d3350a9dc7
Steffen Liebergeld <perl@gmx.org>
2016-06-08 20:22:05 +0200

    MusicStateSpec: introduce new class describing the music state
    Contains:
    - state
    - position
    - playRate
    - shuffle
    - repeat
    
    This is close to what PebbleProtocol currently supports.

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/MusicStateSpec.java
@@ -0,0 +1,12 @@
+package nodomain.freeyourgadget.gadgetbridge.model;
+
+/**
+ * Created by steffen on 07.06.16.
+ */
+public class MusicStateSpec {
+    public byte state;
+    public int position;
+    public int playRate;
+    public byte shuffle;
+    public byte repeat;
+}
