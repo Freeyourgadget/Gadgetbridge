91f374edec601c001288f9018830f2173a11a06c
Steffen Liebergeld <perl@gmx.org>
2016-06-09 20:02:01 +0200

    Revert ""duration" parameter in onSetMusicInfo uses microseconds"
    The decision on granularity of APIs is up to the maintainers.
    
    This reverts commit 204748c518c0477689c05730c30ab57859ef89fd.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/PebbleProtocol.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/PebbleProtocol.java
@@ -1159,7 +1159,7 @@ public class PebbleProtocol extends GBDeviceProtocol {
             }
 
             buf.order(ByteOrder.LITTLE_ENDIAN);
-            buf.putInt(duration);
+            buf.putInt(duration * 1000);
             buf.putShort((short) (trackCount & 0xffff));
             buf.putShort((short) (trackNr & 0xffff));
 
