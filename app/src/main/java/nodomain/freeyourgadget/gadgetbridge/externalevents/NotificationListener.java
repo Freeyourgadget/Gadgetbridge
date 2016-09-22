fd03dac5cd3756c4726c5f78dadeca88fd2d2a4d
Andreas Shimokawa <shimokawa@fsfe.org>
2016-09-22 09:31:14 +0200

    Pebble: try to work around duplicate Telegram messages
    (#395)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/NotificationListener.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/NotificationListener.java
@@ -285,6 +285,11 @@ public class NotificationListener extends NotificationListenerService {
 
         NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender(notification);
         List<NotificationCompat.Action> actions = wearableExtender.getActions();
+
+        if (actions.isEmpty() && notificationSpec.type == NotificationType.TELEGRAM) {
+            return; // workaround for duplicate telegram message
+        }
+
         for (NotificationCompat.Action act : actions) {
             if (act != null && act.getRemoteInputs() != null) {
                 LOG.info("found wearable action: " + act.getTitle() + "  " + sbn.getTag());
