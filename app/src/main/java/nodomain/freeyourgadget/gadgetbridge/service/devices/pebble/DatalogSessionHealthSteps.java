42f622af8510221ae7fdd211a580e1d392785e9f
Daniele Gobbetti <daniele+github@gobbetti.name>
2016-09-30 17:28:46 +0200

    Support record version 12 (firmware 4.1)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/DatalogSessionHealthSteps.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/DatalogSessionHealthSteps.java
@@ -50,7 +50,7 @@ public class DatalogSessionHealthSteps extends DatalogSessionPebbleHealth {
 
             recordVersion = datalogMessage.getShort();
 
-            if ((recordVersion != 5) && (recordVersion != 6) && (recordVersion != 7))
+            if ((recordVersion != 5) && (recordVersion != 6) && (recordVersion != 7) && (recordVersion != 12))
                 return false; //we don't know how to deal with the data TODO: this is not ideal because we will get the same message again and again since we NACK it
 
             timestamp = datalogMessage.getInt();
@@ -101,7 +101,7 @@ public class DatalogSessionHealthSteps extends DatalogSessionPebbleHealth {
     }
 
     private class StepsRecord {
-        byte[] knownVersions = {5, 6, 7};
+        byte[] knownVersions = {5, 6, 7, 12};
         short version;
         int timestamp;
         int steps;
