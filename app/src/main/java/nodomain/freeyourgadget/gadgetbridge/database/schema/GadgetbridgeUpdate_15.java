fa8df9f552265e207b500b91cf9658f53d9c5090
Andreas Shimokawa <shimokawa@fsfe.org>
2016-11-27 10:52:43 +0100

    add missing migration script

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/database/schema/GadgetbridgeUpdate_15.java
@@ -0,0 +1,27 @@
+package nodomain.freeyourgadget.gadgetbridge.database.schema;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import nodomain.freeyourgadget.gadgetbridge.database.DBHelper;
+import nodomain.freeyourgadget.gadgetbridge.database.DBUpdateScript;
+import nodomain.freeyourgadget.gadgetbridge.entities.DeviceAttributesDao;
+import nodomain.freeyourgadget.gadgetbridge.entities.PebbleHealthActivitySampleDao;
+
+/*
+ * adds heart rate column to health table
+ */
+
+public class GadgetbridgeUpdate_15 implements DBUpdateScript {
+    @Override
+    public void upgradeSchema(SQLiteDatabase db) {
+        if (!DBHelper.existsColumn(DeviceAttributesDao.TABLENAME, DeviceAttributesDao.Properties.VolatileIdentifier.columnName, db)) {
+            String ADD_COLUMN_VOLATILE_IDENTIFIER = "ALTER TABLE " + DeviceAttributesDao.TABLENAME + " ADD COLUMN "
+                    + DeviceAttributesDao.Properties.VolatileIdentifier.columnName + " TEXT;";
+            db.execSQL(ADD_COLUMN_VOLATILE_IDENTIFIER);
+        }
+    }
+
+    @Override
+    public void downgradeSchema(SQLiteDatabase db) {
+    }
+}
