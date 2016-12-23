6c186329dff933323500ec4e550a8a7364286b3e
João Paulo Barraca <jpbarraca@gmail.com>
2016-12-23 00:08:14 +0000

    Cleanup HPlusSampleProvider

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/hplus/HPlusSampleProvider.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/hplus/HPlusSampleProvider.java
@@ -25,12 +25,6 @@ import nodomain.freeyourgadget.gadgetbridge.model.ActivityKind;
 
 public class HPlusSampleProvider extends AbstractSampleProvider<HPlusHealthActivitySample> {
 
-    public static final int TYPE_DEEP_SLEEP = 4;
-    public static final int TYPE_LIGHT_SLEEP = 5;
-    public static final int TYPE_ACTIVITY = -1;
-    public static final int TYPE_UNKNOWN = -1;
-    public static final int TYPE_NONWEAR = 3;
-    public static final int TYPE_CHARGING = 6;
 
     private GBDevice mDevice;
     private DaoSession mSession;
@@ -47,37 +41,11 @@ public class HPlusSampleProvider extends AbstractSampleProvider<HPlusHealthActiv
     }
 
     public int normalizeType(int rawType) {
-        switch (rawType) {
-            case TYPE_DEEP_SLEEP:
-                return ActivityKind.TYPE_DEEP_SLEEP;
-            case TYPE_LIGHT_SLEEP:
-                return ActivityKind.TYPE_LIGHT_SLEEP;
-            case TYPE_ACTIVITY:
-                return ActivityKind.TYPE_ACTIVITY;
-            case TYPE_NONWEAR:
-                return ActivityKind.TYPE_NOT_WORN;
-            case TYPE_CHARGING:
-                return ActivityKind.TYPE_NOT_WORN; //I believe it's a safe assumption
-            default:
-//            case TYPE_UNKNOWN: // fall through
-                return ActivityKind.TYPE_UNKNOWN;
-        }
+        return rawType;
     }
 
     public int toRawActivityKind(int activityKind) {
-        switch (activityKind) {
-            case ActivityKind.TYPE_ACTIVITY:
-                return TYPE_ACTIVITY;
-            case ActivityKind.TYPE_DEEP_SLEEP:
-                return TYPE_DEEP_SLEEP;
-            case ActivityKind.TYPE_LIGHT_SLEEP:
-                return TYPE_LIGHT_SLEEP;
-            case ActivityKind.TYPE_NOT_WORN:
-                return TYPE_NONWEAR;
-            case ActivityKind.TYPE_UNKNOWN: // fall through
-            default:
-                return TYPE_UNKNOWN;
-        }
+        return activityKind;
     }
 
 
