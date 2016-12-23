9a338c9baefae05bc66e58bc8d5e009d6835d05c
João Paulo Barraca <jpbarraca@gmail.com>
2016-12-23 01:35:18 +0000

    HPlus: Fix text notification length

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/hplus/HPlusSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/hplus/HPlusSupport.java
@@ -653,11 +653,11 @@ public class HPlusSupport extends AbstractBTLEDeviceSupport {
             String message = "";
 
             if (title != null) {
-                if (title.length() > 12) {
+                if (title.length() > 17) {
                     message = title.substring(0, 12);
                 } else {
                     message = title;
-                    for (int i = message.length(); i < 12; i++)
+                    for (int i = message.length(); i < 17; i++)
                         message += " ";
                 }
             }
