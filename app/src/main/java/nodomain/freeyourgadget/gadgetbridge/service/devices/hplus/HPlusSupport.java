051d1e739094bddde285f0c306bc9f230be39005
João Paulo Barraca <jpbarraca@gmail.com>
2017-01-05 00:17:28 +0000

    HPlus: fix typo

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/hplus/HPlusSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/hplus/HPlusSupport.java
@@ -208,7 +208,7 @@ public class HPlusSupport extends AbstractBTLEDeviceSupport {
     }
 
     private HPlusSupport setCurrentDate(TransactionBuilder transaction) {
-        Calendar c = GregorianGregorianCalendar.getInstance();
+        Calendar c = GregorianCalendar.getInstance();
         int year = c.get(Calendar.YEAR) - 1900;
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
