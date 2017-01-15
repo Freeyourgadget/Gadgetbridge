26a349210e0231370a8deb7f66268e0586fe3937
Daniele Gobbetti <daniele+github@gobbetti.name>
2017-01-15 18:19:30 +0100

    Pebble: make the text in the dummy weather configuration activity visible.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/WeatherNotificationConfig.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/WeatherNotificationConfig.java
@@ -1,16 +1,14 @@
 package nodomain.freeyourgadget.gadgetbridge.externalevents;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.TextView;
 
 import nodomain.freeyourgadget.gadgetbridge.R;
 import nodomain.freeyourgadget.gadgetbridge.activities.GBActivity;
 
 public class WeatherNotificationConfig extends GBActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_notification);
     }
 }
