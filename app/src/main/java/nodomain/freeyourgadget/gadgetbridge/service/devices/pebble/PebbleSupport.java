c0076b20d3f369376dca364b1499edbc0380a0e6
Andreas Shimokawa <shimokawa@fsfe.org>
2017-02-09 17:23:41 +0100

    fix copy and pasta error

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/PebbleSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/PebbleSupport.java
@@ -122,7 +122,7 @@ public class PebbleSupport extends AbstractSerialDeviceSupport {
             notificationSpec.title = null;
             notificationSpec.phoneNumber = null;
         } else if (getContext().getString(R.string.p_pebble_privacy_mode_content).equals(currentPrivacyMode)) {
-            if (notificationSpec.sender != null) {if (notificationSpec.sender != null) {
+            if (notificationSpec.sender != null) {
                 notificationSpec.sender = "\n\n\n\n\n" + notificationSpec.sender;
             } else if (notificationSpec.title != null) {
                 notificationSpec.title = "\n\n\n\n\n" + notificationSpec.title;
