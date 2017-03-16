9beb781c1d159a0367d75af5a6c32d915fe4c0eb
Alberto <albertsal83@gmail.com>
2017-03-16 21:35:49 +0100

    import export SharedPreferences
    i add function for import and export SharedPreferences setting
    
    when export or import db execute export or import SharedPreferences
    
    for blacklist i preload HashSet

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/DbManagementActivity.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/DbManagementActivity.java
@@ -1,27 +1,12 @@
-/*  Copyright (C) 2016-2017 Andreas Shimokawa, Carsten Pfeiffer, Daniele
-    Gobbetti
-
-    This file is part of Gadgetbridge.
-
-    Gadgetbridge is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Gadgetbridge is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.activities;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 
 import nodomain.freeyourgadget.gadgetbridge.GBApplication;
 import nodomain.freeyourgadget.gadgetbridge.R;
@@ -40,10 +26,13 @@ import nodomain.freeyourgadget.gadgetbridge.database.DBHandler;
 import nodomain.freeyourgadget.gadgetbridge.database.DBHelper;
 import nodomain.freeyourgadget.gadgetbridge.util.FileUtils;
 import nodomain.freeyourgadget.gadgetbridge.util.GB;
+import nodomain.freeyourgadget.gadgetbridge.util.ImportExportSharedPreferences;
 
 
 public class DbManagementActivity extends GBActivity {
     private static final Logger LOG = LoggerFactory.getLogger(DbManagementActivity.class);
+    private static SharedPreferences sharedPrefs;
+    private ImportExportSharedPreferences shared_file = new ImportExportSharedPreferences();
 
     private Button exportDBButton;
     private Button importDBButton;
@@ -95,6 +84,8 @@ public class DbManagementActivity extends GBActivity {
                 deleteActivityDatabase();
             }
         });
+
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     private boolean hasOldActivityDatabase() {
@@ -110,8 +101,33 @@ public class DbManagementActivity extends GBActivity {
         return getString(R.string.dbmanagementactivvity_cannot_access_export_path);
     }
 
+    private void exportShared() {
+        // BEGIN EXAMPLE
+        File myPath = null;
+        try {
+            myPath = FileUtils.getExternalFilesDir();
+            File myFile = new File(myPath, "Export_preference");
+            shared_file.exportToFile(sharedPrefs,myFile,null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void importShared() {
+        // BEGIN EXAMPLE
+        File myPath = null;
+        try {
+            myPath = FileUtils.getExternalFilesDir();
+            File myFile = new File(myPath, "Export_preference");
+            shared_file.importFromFile(sharedPrefs,myFile );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void exportDB() {
         try (DBHandler dbHandler = GBApplication.acquireDB()) {
+            exportShared();
             DBHelper helper = new DBHelper(this);
             File dir = FileUtils.getExternalFilesDir();
             File destFile = helper.exportDB(dbHandler, dir);
@@ -130,6 +146,7 @@ public class DbManagementActivity extends GBActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         try (DBHandler dbHandler = GBApplication.acquireDB()) {
+                            importShared();
                             DBHelper helper = new DBHelper(DbManagementActivity.this);
                             File dir = FileUtils.getExternalFilesDir();
                             SQLiteOpenHelper sqLiteOpenHelper = dbHandler.getHelper();
