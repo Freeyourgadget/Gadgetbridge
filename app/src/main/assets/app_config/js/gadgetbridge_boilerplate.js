Freeyourgadget/Gadgetbridge

commit d8894d315ac36c678d84ed5a6d861b907cbe272f

Pebble: In the geolocation override, fail if the position is too old.
The previous logic was wrong as it was calling success every time. Further checks must be added for some watchfaces.

app/src/main/assets/app_config/js/gadgetbridge_boilerplate.js
@@ -1,7 +1,11 @@
-navigator.geolocation.getCurrentPosition = function(success, failure) { //override because default implementation requires GPS permission
-    success(JSON.parse(GBjs.getCurrentPosition()));
-    							failure({ code: 2, message: "POSITION_UNAVAILABLE"});
+navigator.geolocation.getCurrentPosition = function(success, failure, options) { //override because default implementation requires GPS permission
+    geoposition = JSON.parse(GBjs.getCurrentPosition());
 
+    if(options && options.maximumAge && geoposition.timestamp < Date.now() - options.maximumAge) {
+    	failure({ code: 2, message: "POSITION_UNAVAILABLE"});
+    } else {
+        success();
+    }
 }
 
 if (window.Storage){

commit 4dbc255ad5d6313a5a6badd0310362e1e03789ca

Pebble: return the current position to the caller of the getCurrentPosition function.
This fixes a bug introduced in d8894d315ac36c678d84ed5a6d861b907cbe272f that affects also #643.

app/src/main/assets/app_config/js/gadgetbridge_boilerplate.js
@@ -4,7 +4,7 @@ navigator.geolocation.getCurrentPosition = function(success, failure, options) {
     if(options && options.maximumAge && geoposition.timestamp < Date.now() - options.maximumAge) {
     	failure({ code: 2, message: "POSITION_UNAVAILABLE"});
     } else {
-        success();
+        success(geoposition);
     }
 }
 