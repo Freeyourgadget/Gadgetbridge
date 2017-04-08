dd5ee039328204589740e6d75fbb9c1f159e13c9
Daniele Gobbetti <daniele+github@gobbetti.name>
2017-04-08 15:50:13 +0200

    Set the right color for the Pie charts entry labels.
    Closes #647 by showing the text also on the light theme, instead of hiding it everywhere.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/AbstractWeekChartFragment.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/AbstractWeekChartFragment.java
@@ -191,6 +191,7 @@ public abstract class AbstractWeekChartFragment extends AbstractChartFragment {
     private void setupTodayPieChart() {
         mTodayPieChart.setBackgroundColor(BACKGROUND_COLOR);
         mTodayPieChart.getDescription().setTextColor(DESCRIPTION_COLOR);
+        mTodayPieChart.setEntryLabelColor(DESCRIPTION_COLOR);
         mTodayPieChart.getDescription().setText(getPieDescription(mTargetValue));
 //        mTodayPieChart.setNoDataTextDescription("");
         mTodayPieChart.setNoDataText("");
