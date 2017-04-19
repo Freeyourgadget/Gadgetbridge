18157daf468a7203e88b86da068abc7a9bc8c86e
Daniele Gobbetti <daniele+github@gobbetti.name>
2017-04-19 13:23:13 +0200

    Ensure that the Notification listener service gets restarted if crashed.
    This change adds an additional service that checks the status of the NotificationListenerService, and restarts it if it's stale/crashed.
    Crashes happen mostly during development, but were reported also by users.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/GBApplication.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/GBApplication.java
@@ -53,6 +53,7 @@ import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
 import nodomain.freeyourgadget.gadgetbridge.impl.GBDeviceService;
 import nodomain.freeyourgadget.gadgetbridge.model.ActivityUser;
 import nodomain.freeyourgadget.gadgetbridge.model.DeviceService;
+import nodomain.freeyourgadget.gadgetbridge.service.NotificationCollectorMonitorService;
 import nodomain.freeyourgadget.gadgetbridge.util.FileUtils;
 import nodomain.freeyourgadget.gadgetbridge.util.GB;
 import nodomain.freeyourgadget.gadgetbridge.util.GBPrefs;
@@ -146,6 +147,8 @@ public class GBApplication extends Application {
 
         if (isRunningMarshmallowOrLater()) {
             notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            //the following will ensure the notification manager is kept alive
+            startService(new Intent(this, NotificationCollectorMonitorService.class));
         }
     }
 
