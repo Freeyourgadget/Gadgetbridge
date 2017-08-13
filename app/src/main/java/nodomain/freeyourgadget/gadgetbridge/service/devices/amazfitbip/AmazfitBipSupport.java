80dce953720aa2cd2e947559c06373d047d023a7
Andreas Shimokawa <shimokawa@fsfe.org>
2017-08-13 16:31:11 +0200

    Inital Amazfit Bip support
    Support is almost on Mi Band 2 level.
    
    What does not work yet:
    - flashing firmware files
    - taking or rejecting phone calls
    - syncing GPS tracks
    - sending weather
    - notification only include title, not a body
    - unknown notification's text is not forwarded to the watch at all (same on Mi Band 2 #754)

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/amazfitbip/AmazfitBipSupport.java
@@ -0,0 +1,32 @@
+/*  Copyright (C) 2017 Andreas Shimokawa
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.gadgetbridge.service.devices.amazfitbip;
+
+import nodomain.freeyourgadget.gadgetbridge.service.devices.miband.NotificationStrategy;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.miband2.MiBand2Support;
+
+public class AmazfitBipSupport extends MiBand2Support {
+    @Override
+    public NotificationStrategy getNotificationStrategy() {
+        return new AmazfitBipTextNotificationStrategy(this);
+    }
+
+    @Override
+    public void onFindDevice(boolean start) {
+        // Prevent notification spamming from MiBand2Support for now
+    }
+}
--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/miband2/MiBand2Support.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/miband2/MiBand2Support.java
@@ -301,7 +301,7 @@ public class MiBand2Support extends AbstractBTLEDeviceSupport {
         return this;
     }
 
-    private NotificationStrategy getNotificationStrategy() {
+    public NotificationStrategy getNotificationStrategy() {
         String firmwareVersion = getDevice().getFirmwareVersion();
         if (firmwareVersion != null) {
             Version ver = new Version(firmwareVersion);
