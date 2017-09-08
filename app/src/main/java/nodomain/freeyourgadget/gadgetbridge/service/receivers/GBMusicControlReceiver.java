a17347c8338b32ee8b61b1100cebbdf156492c5d
Gabe Schrecker <gabe@pbrb.co.uk>
2017-09-08 19:50:04 +0100

    Added Lollipop only detection of the playing media package, using MediaSessionManager. Replicates funtionality of the Official Pebble client.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/receivers/GBMusicControlReceiver.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/receivers/GBMusicControlReceiver.java
@@ -18,17 +18,24 @@
 package nodomain.freeyourgadget.gadgetbridge.service.receivers;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.session.MediaController;
+import android.media.session.MediaSessionManager;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.KeyEvent;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 import nodomain.freeyourgadget.gadgetbridge.GBApplication;
 import nodomain.freeyourgadget.gadgetbridge.deviceevents.GBDeviceEventMusicControl;
+import nodomain.freeyourgadget.gadgetbridge.externalevents.NotificationListener;
 import nodomain.freeyourgadget.gadgetbridge.util.Prefs;
 
 public class GBMusicControlReceiver extends BroadcastReceiver {
@@ -72,6 +79,23 @@ public class GBMusicControlReceiver extends BroadcastReceiver {
         if (keyCode != -1) {
             Prefs prefs = GBApplication.getPrefs();
             String audioPlayer = prefs.getString("audio_player", "default");
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                MediaSessionManager mediaSessionManager =
+                        (MediaSessionManager) context.getSystemService(Context.MEDIA_SESSION_SERVICE);
+
+                List<MediaController> controllers = mediaSessionManager.getActiveSessions(
+                        new ComponentName(context, NotificationListener.class));
+                try {
+                    MediaController controller = controllers.get(0);
+                    audioPlayer = controller.getPackageName();
+                } catch (IndexOutOfBoundsException e) {
+                    System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+                }
+            }
+
+            Log.d("keypress", musicCmd.toString());
+            Log.d("sent to", audioPlayer);
 
             long eventtime = SystemClock.uptimeMillis();
 
