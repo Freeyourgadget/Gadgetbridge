ac981115027548c02fd41edced0cbd99867a29f9
Andreas Shimokawa <shimokawa@fsfe.org>
2017-11-05 00:08:39 +0100

    Amazfit Bip: Fix Running/Cycling activity mapping (was swapped)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/amazfitbip/BipActivityType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/amazfitbip/BipActivityType.java
@@ -3,10 +3,10 @@ package nodomain.freeyourgadget.gadgetbridge.service.devices.amazfitbip;
 import nodomain.freeyourgadget.gadgetbridge.model.ActivityKind;
 
 public enum BipActivityType {
-    Outdoor(4),
+    Outdoor(1),
     Treadmill(2),
-    Cycling(1),
-    Walking(3);
+    Walking(3),
+    Cycling(4);
 
     private final int code;
 
