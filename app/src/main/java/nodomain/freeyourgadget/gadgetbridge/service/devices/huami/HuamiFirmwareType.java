6b56e8e568bb3b0f5d66fe8806e7722000dde460
Andreas Shimokawa <shimokawa@fsfe.org>
2017-11-06 07:17:01 +0100

    Amazfit Cor: Fix flashing of new .res files

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/HuamiFirmwareType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/HuamiFirmwareType.java
@@ -20,6 +20,7 @@ public enum HuamiFirmwareType {
     FIRMWARE((byte) 0),
     FONT((byte) 1),
     RES((byte) 2),
+    RES_NEW((byte)130),
     GPS((byte) 3),
     GPS_CEP((byte) 4),
     GPS_ALMANAC((byte)5),
