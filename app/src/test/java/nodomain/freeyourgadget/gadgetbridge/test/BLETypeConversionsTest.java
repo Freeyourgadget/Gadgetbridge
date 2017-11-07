b76e78768c41c08d90f8314795bf7f14084c7c3a
cpfeiffer <cpfeiffer@users.noreply.github.com>
2017-11-07 21:23:54 +0100

    Another small tz+dst parsing testcase

--- app/src/test/java/nodomain/freeyourgadget/gadgetbridge/test/BLETypeConversionsTest.java
+++ app/src/test/java/nodomain/freeyourgadget/gadgetbridge/test/BLETypeConversionsTest.java
@@ -10,12 +10,26 @@ import static org.junit.Assert.assertTrue;
 
 public class BLETypeConversionsTest extends TestBase {
     @Test
-    public void testTimeParsing() {
+    public void testTimeParsing1() {
         byte[] requested = new byte[] {
                 (byte) 0xe1, 0x07, 0x0a, 0x1d, 0x00, 0x1c, 0x00,0x08
         };
         byte[] received = new byte[] {
                 (byte) 0xe1, 0x07, 0x0a, 0x1c, 0x17, 0x1c, 0x00, 0x04
+        };
+        GregorianCalendar calRequested = BLETypeConversions.rawBytesToCalendar(requested, false);
+        GregorianCalendar calReceived = BLETypeConversions.rawBytesToCalendar(received, false);
+
+        assertTrue(calRequested.getTime().equals(calReceived.getTime()));
+    }
+
+    @Test
+    public void testTimeParsingWithDST() {
+        byte[] requested = new byte[] {
+                (byte) 0xe1,0x07,0x0a,0x09,0x11,0x23,0x00,0x08
+        };
+        byte[] received = new byte[] {
+                (byte) 0xe1,0x07,0x0a,0x09,0x10,0x23,0x00,0x04
         };
         GregorianCalendar calRequested = BLETypeConversions.rawBytesToCalendar(requested, false);
         GregorianCalendar calReceived = BLETypeConversions.rawBytesToCalendar(received, false);
