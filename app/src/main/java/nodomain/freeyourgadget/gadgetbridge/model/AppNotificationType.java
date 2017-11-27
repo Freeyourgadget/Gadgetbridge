04c917f8632ab53df967e13b14e36971fe7d383a
Tomas Radej <tradej@redhat.com>
2017-11-27 16:44:06 +0100

    Added Etar as calendar app type

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/AppNotificationType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/AppNotificationType.java
@@ -138,6 +138,9 @@ public class AppNotificationType extends HashMap<String, NotificationType> {
 
         // Transit
         put("com.thetransitapp.droid", NotificationType.TRANSIT);
+
+        // Etar
+        put("ws.xsoh.etar", NotificationType.GENERIC_CALENDAR);
     }
 
 }
