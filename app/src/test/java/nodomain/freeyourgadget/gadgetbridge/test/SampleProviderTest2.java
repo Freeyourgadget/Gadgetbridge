5ae72b3d04588faa5aa66457fd6c21fbcb239afd
Benedikt Elser <benedikt.elser@th-deg.de>
2018-07-12 22:47:55 +0200

    Debug

---
+++ app/src/test/java/nodomain/freeyourgadget/gadgetbridge/test/SampleProviderTest2.java
@@ -0,0 +1,55 @@
+package nodomain.freeyourgadget.gadgetbridge.database;
+
+import android.content.ContentResolver;
+import android.util.Log;
+
+import org.junit.Test;
+
+import nodomain.freeyourgadget.gadgetbridge.GBApplication;
+import nodomain.freeyourgadget.gadgetbridge.contentprovider.HRContentProvider;
+import nodomain.freeyourgadget.gadgetbridge.contentprovider.HRContentProviderContract;
+import nodomain.freeyourgadget.gadgetbridge.devices.DeviceManager;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.test.TestBase;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.robolectric.shadows.ShadowContentResolver;
+import org.robolectric.shadows.ShadowLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class SampleProviderTest2 extends TestBase {
+    private static final Logger LOG = LoggerFactory.getLogger(SampleProviderTest2.class);
+
+    private GBDevice dummyGBDevice;
+    private ContentResolver mContentResolver;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        ShadowLog.stream = System.out; // show logger’s output
+
+        dummyGBDevice = createDummyGDevice("00:00:00:00:10");
+        mContentResolver = app.getContentResolver();
+
+        HRContentProvider provider = new HRContentProvider();
+
+        // Stuff context into provider
+        provider.attachInfo(app.getApplicationContext(), null);
+
+        ShadowContentResolver.registerProviderInternal(HRContentProviderContract.AUTHORITY, provider);
+    }
+
+   @Test
+   public void testDeviceManager() {
+       DeviceManager manager = ((GBApplication) (this.getContext())).getDeviceManager();
+       Log.d("---------------", "-----------------------------------");
+
+       System.out.println("-----------------------------------------");
+       assertNotNull(((GBApplication) GBApplication.getContext()).getDeviceManager());
+       LOG.debug(manager.toString());
+
+   }
+}
