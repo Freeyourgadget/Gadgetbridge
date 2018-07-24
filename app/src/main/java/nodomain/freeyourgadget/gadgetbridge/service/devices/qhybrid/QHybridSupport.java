943864e7f1e133e10c3f8d61a0c190a4faf339cb
dakhnod <dakhnod@gmail.com>
2018-07-24 03:10:56 +0200

    changed response filter strategy and added som getters

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/QHybridSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/QHybridSupport.java
@@ -23,21 +23,26 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.TimeZone;
 import java.util.UUID;
 
 import d.d.qhook.requests.displayPairAnimationRequest;
 import d.d.qhook.requests.fileListRequest;
 import d.d.qhook.requests.getBatteryRequest;
+import d.d.qhook.requests.getGoalInStepsRequest;
 import d.d.qhook.requests.getVibeStrengthRequest;
 import d.d.qhook.requests.playNotificationFilterVibrationRequest;
 import d.d.qhook.requests.sendNotificationHandControlRequest;
 import d.d.qhook.requests.setCurrentTimeServiceRequest;
+import d.d.qhook.requests.setGoalInStepsRequest;
 import d.d.qhook.requests.setMovingHandsRequest;
 import d.d.qhook.requests.setReleaseHandsControlRequest;
 import d.d.qhook.requests.setRequestHandsControlRequest;
+import d.d.qhook.requests.setVibeStrengthRequest;
 import nodomain.freeyourgadget.gadgetbridge.devices.qhybrid.PackageConfigHelper;
 import nodomain.freeyourgadget.gadgetbridge.devices.qhybrid.PackageConfig;
 import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
@@ -66,15 +71,29 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
         super(logger);
     }
 
-    ArrayList<ew> requests = new ArrayList<>();
+    //ArrayList<ew> requests = new ArrayList<>();
+    List<ResponseFilter> responseFilters;
 
     OnVibrationStrengthListener vibrationStrengthListener;
+    OnGoalListener goalListener;
 
     public static final String commandControl = "qhybrid_command_control";
     public static final String commandUncontrol = "qhybrid_command_uncontrol";
     public static final String commandSet = "qhybrid_command_set";
     public static final String commandVibrate = "qhybrid_command_vibrate";
     public static final String commandUpdate = "qhybrid_command_update";
+
+    class ResponseFilter {
+        int responseDataLength;
+        byte[] responseDataMask;
+        Class<? extends com.misfit.ble.obfuscated.ew> responseClass;
+
+        public ResponseFilter(Class<? extends ew> responseClass, int responseDataLanegth, byte... responseDataMask) {
+            this.responseDataLength = responseDataLanegth;
+            this.responseDataMask = responseDataMask;
+            this.responseClass = responseClass;
+        }
+    }
 
 
     public QHybridSupport() {
@@ -87,6 +106,12 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
         commandFilter.addAction(commandUpdate);
         LocalBroadcastManager.getInstance(getContext()).registerReceiver(commandReceiver, commandFilter);
         //addSupportedService(UUID.fromString("3dda0002-957f-7d4a-34a6-74696673696d"));
+
+        responseFilters = Arrays.asList(
+                new ResponseFilter(getGoalInStepsRequest.class, 6, (byte) 3, (byte) 16),
+                new ResponseFilter(getVibeStrengthRequest.class, 4, (byte) 3, (byte) 15, (byte) 8),
+                new ResponseFilter(getBatteryRequest.class, 3, (byte) 3, (byte) 8)
+        );
     }
 
     @Override
@@ -97,23 +122,39 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
 
     private void getBattery() {
         d.d.qhook.requests.getBatteryRequest request = new getBatteryRequest();
-        requests.add(request);
         request.cb();
         queueWrite(request);
     }
 
-    public void getVibrationStrength(OnVibrationStrengthListener listener){
+    public void getGoal(OnGoalListener listener) {
+        this.goalListener = listener;
+        d.d.qhook.requests.getGoalInStepsRequest request = new getGoalInStepsRequest();
+        request.cb();
+        queueWrite(request);
+    }
+
+    public void setGoal(long goal) {
+        d.d.qhook.requests.setGoalInStepsRequest request = new setGoalInStepsRequest();
+        request.f(goal);
+        queueWrite(request);
+    }
+
+    public void getVibrationStrength(OnVibrationStrengthListener listener) {
         this.vibrationStrengthListener = listener;
         d.d.qhook.requests.getVibeStrengthRequest request = new getVibeStrengthRequest();
         request.cb();
-        requests.add(request);
+        queueWrite(request);
+    }
+
+    public void setVibrationStrength(int strength) {
+        d.d.qhook.requests.setVibeStrengthRequest request = new setVibeStrengthRequest();
+        request.c((short) strength);
         queueWrite(request);
     }
 
     private void getXY() {
         d.d.qhook.requests.fileListRequest request = new fileListRequest();
         request.cb();
-        requests.add(request);
 
         queueWrite(request);
     }
@@ -161,7 +202,7 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
 
         playPairingAnimation();
         getBattery();
-        getXY();
+        //getXY();
 
         logger.debug("onServicesDiscovered");
     }
@@ -185,17 +226,19 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
         String packageName = notificationSpec.sourceName;
 
         PackageConfig config = helper.getSetting(packageName);
-        if(config == null) return;
+        if (config == null) return;
 
         Log.d("Service", "handling notification");
 
         int mode = ((AudioManager) getContext().getApplicationContext().getSystemService(Context.AUDIO_SERVICE)).getRingerMode();
-        if(mode == AudioManager.RINGER_MODE_SILENT && config.getRespectSilentMode()) return;
+        if (mode == AudioManager.RINGER_MODE_SILENT && config.getRespectSilentMode()) return;
 
 
         ArrayList<HandControl> controls = new ArrayList<>(2);
-        if (config.getHour() != -1) controls.add(new HandControl(SAMEnum.HandID.HOUR, (short)config.getHour()));
-        if (config.getMin() != -1) controls.add(new HandControl(SAMEnum.HandID.MINUTE, (short)config.getMin()));
+        if (config.getHour() != -1)
+            controls.add(new HandControl(SAMEnum.HandID.HOUR, (short) config.getHour()));
+        if (config.getMin() != -1)
+            controls.add(new HandControl(SAMEnum.HandID.MINUTE, (short) config.getMin()));
 
         SAMEnum.VibeEnum vibe = getVine(config.getVibration());
 
@@ -373,36 +416,69 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
 
     @Override
     public boolean onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-        if (requests.size() > 0) {
-            if (requests.get(0).getRequestName().equals("fileList")) {
+            /*if (requests.get(0).getRequestName().equals("fileList")) {
                 if (characteristic.getUuid().toString().equals("3dda0004-957f-7d4a-34a6-74696673696d")) {
                     requests.get(0).handleResponse(characteristic.getUuid().toString(), characteristic.getValue());
-                    return super.onCharacteristicChanged(gatt, characteristic);
+                    return true;
                 }
-            }
-            ew lastRequest = requests.remove(0);
-            lastRequest.handleResponse(characteristic.getUuid().toString(), characteristic.getValue());
-            logger.debug("response: " + lastRequest.getResponseDescriptionJSON().toString());
+            }*/
+        //if(characteristic.getValue().length == 3 && characteristic.getValue()[0] == 1 && characteristic.getValue()[1] == 15 && characteristic.getValue()[2] == 8) return true;
+        com.misfit.ble.obfuscated.ew request = resolveAnswer(characteristic);
+        String values = String.valueOf(characteristic.getValue()[0]);
+        for (int i = 1; i < characteristic.getValue().length; i++) {
+            values += ", " + String.valueOf(characteristic.getValue()[i]);
+        }
+        if (request == null) {
+            Log.d("Service", "unable to resolve " + characteristic.getUuid().toString() + ": " + values);
+            return true;
+        }
+        request.handleResponse(characteristic.getUuid().toString(), characteristic.getValue());
 
-            switch (lastRequest.getRequestName()) {
-                case "getBattery": {
-                    gbDevice.setBatteryLevel(((getBatteryRequest.a) lastRequest.getResponse()).ly);
-                    logger.debug("battery level: " + gbDevice.getBatteryLevel());
-                    break;
+        logger.debug(request.getRequestName() + " response: " + request.getResponseDescriptionJSON().toString() + "   " + values);
+
+        switch (request.getRequestName()) {
+            case "getBattery": {
+                gbDevice.setBatteryLevel(((getBatteryRequest.a) request.getResponse()).ly);
+                logger.debug("battery level: " + gbDevice.getBatteryLevel());
+                break;
+            }
+            case "getVibeStrength": {
+                if (this.vibrationStrengthListener != null) {
+                    this.vibrationStrengthListener.onVibrationStrength(((getVibeStrengthRequest) request).dm().nH);
+                    this.vibrationStrengthListener = null;
                 }
-                case "getVibeStrength":{
-                    if(this.vibrationStrengthListener != null){
-                        this.vibrationStrengthListener.onVibrationStrength(((getVibeStrengthRequest)lastRequest).dm().nH);
-                        this.vibrationStrengthListener = null;
-                    }
-                    break;
+                break;
+            }
+            case "getGoalInSteps": {
+                if (this.goalListener != null) {
+                    this.goalListener.onGoal(((getGoalInStepsRequest) request).cI().mb);
+                    this.goalListener = null;
                 }
+                break;
             }
         }
 
-
         return super.onCharacteristicChanged(gatt, characteristic);
 
+    }
+
+    private com.misfit.ble.obfuscated.ew resolveAnswer(BluetoothGattCharacteristic characteristic) {
+        byte[] values = characteristic.getValue();
+        for (ResponseFilter filter : responseFilters) {
+            if (filter.responseDataLength != values.length) continue;
+            for (int i = 0; i < filter.responseDataMask.length; i++) {
+                if (values[i] != filter.responseDataMask[i]) continue;
+            }
+            try {
+                return filter.responseClass.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 
     @Override
@@ -424,15 +500,15 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
         queueWrite(request);
     }
 
-    private void vibrate(int vibration){
+    void vibrate(int vibration) {
         sendNotificationHandControlRequest request = new sendNotificationHandControlRequest();
-        request.a(QMotionEnum.LEDColor.BLUE, (byte)1, getVine(vibration), 0, Collections.emptyList());
+        request.a(QMotionEnum.LEDColor.BLUE, (byte) 1, getVine(vibration), 0, Collections.emptyList());
         queueWrite(request);
     }
 
-    private SAMEnum.VibeEnum getVine(int vibration){
-        for(SAMEnum.VibeEnum v : SAMEnum.VibeEnum.values()){
-            if(v.getId() == vibration){
+    private SAMEnum.VibeEnum getVine(int vibration) {
+        for (SAMEnum.VibeEnum v : SAMEnum.VibeEnum.values()) {
+            if (v.getId() == vibration) {
                 return v;
             }
         }
@@ -464,7 +540,7 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
 
                     break;
                 }
-                case commandVibrate:{
+                case commandVibrate: {
                     vibrate(config.getVibration());
                     break;
                 }
@@ -476,7 +552,11 @@ public class QHybridSupport extends AbstractBTLEDeviceSupport {
         }
     };
 
-    public interface OnVibrationStrengthListener{
+    public interface OnVibrationStrengthListener {
         public void onVibrationStrength(int strength);
+    }
+
+    public interface OnGoalListener {
+        public void onGoal(long goal);
     }
 }
