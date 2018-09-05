fa2186872f1ccd5589c4e8e32426028a8edf7c94
Roi <roigreenberg@gmail.com>
2018-09-05 22:40:56 +0300

    fix tests

--- app/src/test/java/nodomain/freeyourgadget/gadgetbridge/test/RtlUtilsTest.java
+++ app/src/test/java/nodomain/freeyourgadget/gadgetbridge/test/RtlUtilsTest.java
@@ -108,7 +108,7 @@ public class RtlUtilsTest extends TestBase {
         boolean result = RtlUtils.isLtr(english1);
         assertTrue("Is Ltr Character failed", result);
 
-        result = RtlUtils.isLtr(english1);
+        result = RtlUtils.isLtr(english2);
         assertTrue("Is Ltr Character failed", result);
 
         result = RtlUtils.isLtr(hebrew1);
@@ -138,7 +138,7 @@ public class RtlUtilsTest extends TestBase {
         boolean result = RtlUtils.isPunctuations(punctuation1);
         assertTrue("Is Punctuation Character failed", result);
 
-        result = RtlUtils.isPunctuations(punctuation1);
+        result = RtlUtils.isPunctuations(punctuation2);
         assertTrue("Is Punctuation Character failed", result);
 
         result = RtlUtils.isPunctuations(english1);
