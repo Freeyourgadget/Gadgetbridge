7127a28456c520d3e7de2bf740370242d967d355
Mormegil <mormegil@centrum.cz>
2018-11-30 16:48:54 +0100

    Enable No1F1 support for a Chinese clone
    This cheap Chinese dummy-smart-watch apparently uses the same chip/protocol, but it reports with a different name, so we need to add it to the hardcoded name-based detection.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/no1f1/No1F1Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/no1f1/No1F1Coordinator.java
@@ -59,7 +59,7 @@ public class No1F1Coordinator extends AbstractDeviceCoordinator {
     @Override
     public DeviceType getSupportedType(GBDeviceCandidate candidate) {
         String name = candidate.getDevice().getName();
-        if (name != null && name.startsWith("X-RUN")) {
+        if (name != null && (name.startsWith("X-RUN") || name.startsWith("MH30"))) {
             return DeviceType.NO1F1;
         }
 
