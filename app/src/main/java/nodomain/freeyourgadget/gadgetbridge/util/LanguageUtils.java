bcca8a0a8d1718004ae619b6853e8533e55426e7
M. Hadi <hhhadddi@yahoo.com>
2019-01-07 21:09:12 +0100

    Added some more Persian characters

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/util/LanguageUtils.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/util/LanguageUtils.java
@@ -79,8 +79,10 @@ public class LanguageUtils {
             put('آ', "2"); put('ئ', "2"); put('إ', "2"); put('ؤ', "2"); put('أ', "2"); put('ء', "2");
 
             // Persian(Farsi)
-            put('پ', "p"); put('چ', "ch"); put('ژ', "zh"); put('ک', "k"); put('گ', "g"); put('ی', "y");
-	    put('؟', "?"); put('٪', "%"); put('؛', ";"); put('،', ","); 
+            put('پ', "p"); put('چ', "ch"); put('ژ', "zh"); put('ک', "k"); put('گ', "g"); put('ی', "y"); put('‌', " "); 
+            put('؟', "?"); put('٪', "%"); put('؛', ";"); put('،', ","); put('۱', "1"); put('۲', "2"); put('۳', "3"); 
+            put('۴', "4"); put('۵', "5"); put('۶', "6"); put('۷', "7"); put('۸', "8"); put('۹', "9"); put('۰', "0"); 
+	    put('»', "<"); put('«', ">"); put('ِ', "e"); put('َ', "a"); put('ُ', "o"); put('ّ', ""); put('هٔ', "h.ie");
 
             // Polish
             put('Ł', "L"); put('ł', "l");
