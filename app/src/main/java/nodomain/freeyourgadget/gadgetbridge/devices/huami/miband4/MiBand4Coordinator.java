261ddee7077cb1993ef86d52942b7ac9cbf9f1fb
Andreas Shimokawa <shimokawa@fsfe.org>
2019-07-26 21:05:16 +0200

    Mi Band 4: Support setting date format (for built-in watchfaces)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/miband4/MiBand4Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/miband4/MiBand4Coordinator.java
@@ -83,6 +83,7 @@ public class MiBand4Coordinator extends HuamiCoordinator {
     public int[] getSupportedDeviceSpecificSettings(GBDevice device) {
         return new int[]{
                 R.xml.devicesettings_miband3,
+                R.xml.devicesettings_dateformat,
                 R.xml.devicesettings_nightmode,
                 R.xml.devicesettings_donotdisturb_withauto,
                 R.xml.devicesettings_liftwrist_display,
