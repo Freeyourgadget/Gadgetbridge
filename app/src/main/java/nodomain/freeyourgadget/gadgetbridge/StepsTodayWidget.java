afe42b6f0b373ba713559c526b1bd009dd7b9053
vanous <petr@linuks.cz>
2019-08-14 23:46:52 +0200

    Trying to retrive data via DailySteps class.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/StepsTodayWidget.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/StepsTodayWidget.java
@@ -16,81 +16,69 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import nodomain.freeyourgadget.gadgetbridge.activities.ConfigureAlarms;
-
-import nodomain.freeyourgadget.gadgetbridge.database.DBHandler;
-import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCoordinator;
-import nodomain.freeyourgadget.gadgetbridge.devices.SampleProvider;
-import nodomain.freeyourgadget.gadgetbridge.entities.AbstractActivitySample;
 import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
-import nodomain.freeyourgadget.gadgetbridge.model.ActivitySample;
-import nodomain.freeyourgadget.gadgetbridge.model.ActivityUser;
+import nodomain.freeyourgadget.gadgetbridge.model.DailySteps;
+import android.content.ComponentName;
 
-import nodomain.freeyourgadget.gadgetbridge.model.Alarm;
-import nodomain.freeyourgadget.gadgetbridge.util.AlarmUtils;
-import nodomain.freeyourgadget.gadgetbridge.util.DeviceHelper;
-import nodomain.freeyourgadget.gadgetbridge.util.GB;
-
-/**
- * Implementation of SleepAlarmWidget functionality. When pressing the widget, an alarm will be set
- * to trigger after a predefined number of hours. A toast will confirm the user about this. The
- * value is retrieved using ActivityUser.().getSleepDuration().
- */
 
 public class StepsTodayWidget extends AppWidgetProvider {
 
-    /**
-     * This is our dedicated action to detect when the widget has been clicked.
-     */
+    public static String getData(Context appContext, GBDevice device){
+        Logger LOG = LoggerFactory.getLogger(StepsTodayWidget.class);
+        LOG.info("PETR get data device: "+ device);
 
+        DailySteps ds = new DailySteps();
+        LOG.info("PETR " + String.valueOf(ds));
+        String kroky = ds.loadItems(device);
+        LOG.info("PETR  →" + kroky);
+        return kroky;
+
+
+
+    };
 
     public static final String ACTION =
-            "nodomain.freeyourgadget.gadgetbridge.SLEEP_ALARM_WIDGET_CLICK";
+            "nodomain.freeyourgadget.gadgetbridge.STEPS_TODAY_CLICK";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-
         Logger LOG = LoggerFactory.getLogger(StepsTodayWidget.class);
-        LOG.info("PETR TEST");
 
+        LOG.info("PETR running updateWidget");
 
-
-        //List<ActivitySample> samples2 = new ArrayList<>();
-        //for (int i = 0; i < samples2.size(); i++) {
-          //  LOG.info(String.valueOf(samples2.get(i)));
-        //}
-
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.steps_today_widget);
 
         // Add our own click intent
         Intent intent = new Intent(context, StepsTodayWidget.class);
+        intent.setAction(ACTION);
+        PendingIntent clickPI = PendingIntent.getBroadcast(
+                context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.stepstodaywidget_text, clickPI);
 
-        views.setTextViewText(R.id.stepstodaywidget_text, String.valueOf(GBApplication.getPrefs().getInt(ActivityUser.PREF_USER_STEPS_GOAL, ActivityUser.defaultUserStepsGoal)));
+
+
+        Context appContext = context.getApplicationContext();
+
+        if (appContext instanceof GBApplication) {
+            GBApplication gbApp = (GBApplication) appContext;
+            GBDevice selectedDevice = gbApp.getDeviceManager().getDevices().get(0);
+            views.setTextViewText(R.id.stepstodaywidget_text,getData(appContext,selectedDevice));
+        }
+
+
+
+
+        //views.setTextViewText(R.id.stepstodaywidget_text, String.valueOf(GBApplication.getPrefs().getInt(ActivityUser.PREF_USER_STEPS_GOAL, ActivityUser.defaultUserStepsGoal)));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -118,65 +106,18 @@ public class StepsTodayWidget extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
         if (ACTION.equals(intent.getAction())) {
-            int userSleepDuration = new ActivityUser().getSleepDuration();
-            // current timestamp
-            GregorianCalendar calendar = new GregorianCalendar();
-            // add preferred sleep duration
-            if (userSleepDuration > 0) {
-                calendar.add(Calendar.HOUR_OF_DAY, userSleepDuration);
-            } else { // probably testing
-                calendar.add(Calendar.MINUTE, 1);
-            }
+            Logger LOG = LoggerFactory.getLogger(StepsTodayWidget.class);
+            LOG.info("PETR onReceive, click");
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), StepsTodayWidget.class.getName());
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
 
-            // overwrite the first alarm and activate it, without
+            onUpdate(context, appWidgetManager, appWidgetIds);
 
-            Context appContext = context.getApplicationContext();
-            if (appContext instanceof GBApplication) {
-                GBApplication gbApp = (GBApplication) appContext;
-                GBDevice selectedDevice = gbApp.getDeviceManager().getSelectedDevice();
-                if (selectedDevice == null || !selectedDevice.isInitialized()) {
-                    GB.toast(context,
-                            context.getString(R.string.appwidget_not_connected),
-                            Toast.LENGTH_LONG, GB.WARN);
-                    return;
-                }
-            }
 
-            int hours = calendar.get(Calendar.HOUR_OF_DAY);
-            int minutes = calendar.get(Calendar.MINUTE);
-
-            GB.toast(context,
-                    context.getString(R.string.appwidget_setting_alarm, hours, minutes),
-                    Toast.LENGTH_SHORT, GB.INFO);
-
-            Alarm alarm = AlarmUtils.createSingleShot(0,true, calendar);
-            ArrayList<Alarm> alarms = new ArrayList<>(1);
-            alarms.add(alarm);
-            GBApplication.deviceService().onSetAlarms(alarms);
-
-//            if (GBApplication.isRunningLollipopOrLater()) {
-//                setAlarmViaAlarmManager(context, calendar.getTimeInMillis());
-//            }
         }
     }
 
-    /**
-     * Use the Android alarm manager to create the alarm icon in the status bar.
-     *
-     * @param packageContext {@code Context}: A Context of the application package implementing this
-     *                       class.
-     * @param triggerTime    {@code long}: time at which the underlying alarm is triggered in wall time
-     *                       milliseconds since the epoch
-     */
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void setAlarmViaAlarmManager(Context packageContext, long triggerTime) {
-        AlarmManager am = (AlarmManager) packageContext.getSystemService(Context.ALARM_SERVICE);
-        // TODO: launch the alarm configuration activity when clicking the alarm in the status bar
-        Intent intent = new Intent(packageContext, ConfigureAlarms.class);
-        PendingIntent pi = PendingIntent.getBroadcast(packageContext, 0, intent,
-                PendingIntent.FLAG_CANCEL_CURRENT);
-        am.setAlarmClock(new AlarmManager.AlarmClockInfo(triggerTime, pi), pi);
-    }
 
     //Calendar day = GregorianCalendar.getInstance();
     //GBDevice device = null;
--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DailySteps.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DailySteps.java
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2015-2019 Andreas Shimokawa, Carsten Pfeiffer, Pavel Elagin
+/*  Copyright (C) 2017-2019 Carsten Pfeiffer, Daniele Gobbetti
 
     This file is part of Gadgetbridge.
 
@@ -16,12 +16,90 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.model;
 
+import android.app.Activity;
 import android.content.Context;
+import android.widget.Toast;
 
-import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import nodomain.freeyourgadget.gadgetbridge.R;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import nodomain.freeyourgadget.gadgetbridge.GBApplication;
+import nodomain.freeyourgadget.gadgetbridge.database.DBHandler;
+import nodomain.freeyourgadget.gadgetbridge.entities.AbstractActivitySample;
+import nodomain.freeyourgadget.gadgetbridge.devices.SampleProvider;
+import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.util.DeviceHelper;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.util.GB;
 
 public class DailySteps {
+    Logger LOG = LoggerFactory.getLogger(DailySteps.class);
+
+    public String loadItems(GBDevice device) {
+
+
+        try (DBHandler handler = GBApplication.acquireDB()) {
+
+            LOG.info("PETR db load items handler " + handler);
+            LOG.info("PETR db load items device " + device);
+            List r = getSamplesOfDay(handler, device);
+            LOG.info("PETR this is r",String.valueOf(r));
+            LOG.info("PETR length of r ",String.valueOf(r.size()));
+            return "test this";
+        } catch (Exception e) {
+
+            GB.toast("Error loading activity summaries.", Toast.LENGTH_SHORT, GB.ERROR, e);
+            return "fail " + e;
+        }
 
     }
+
+
+
+    private List<? extends ActivitySample> getSamplesOfDay(DBHandler db, GBDevice device) {
+
+        Calendar day = GregorianCalendar.getInstance();
+        int startTs;
+        int endTs;
+
+        day = (Calendar) day.clone(); // do not modify the caller's argument
+        day.set(Calendar.HOUR_OF_DAY, 0);
+        day.set(Calendar.MINUTE, 0);
+        day.set(Calendar.SECOND, 0);
+        startTs = (int) (day.getTimeInMillis() / 1000);
+
+        day.set(Calendar.HOUR_OF_DAY, 23);
+        day.set(Calendar.MINUTE, 59);
+        day.set(Calendar.SECOND, 59);
+        endTs = (int) (day.getTimeInMillis() / 1000);
+        LOG.info("PETR get samples " +" : "+ db +" : "+ device +" : "+ startTs +" : "+ endTs);
+        LOG.info("PETR samples size: ", getSamples(db, device, startTs, endTs).size());
+        return getSamples(db, device, startTs, endTs);
+    }
+
+
+    protected List<? extends ActivitySample> getSamples(DBHandler db, GBDevice device, int tsFrom, int tsTo) {
+        LOG.info("PETR get all samples" + db + device + tsFrom + tsTo);
+        return getAllSamples(db, device, tsFrom, tsTo);
+    }
+
+
+    protected SampleProvider<? extends AbstractActivitySample> getProvider(DBHandler db, GBDevice device) {
+        DeviceCoordinator coordinator = DeviceHelper.getInstance().getCoordinator(device);
+        return coordinator.getSampleProvider(device, db.getDaoSession());
+    }
+
+
+    protected List<? extends ActivitySample> getAllSamples(DBHandler db, GBDevice device, int tsFrom, int tsTo) {
+        SampleProvider<? extends ActivitySample> provider = getProvider(db, device);
+        LOG.info("PETR get all activity samples" + db + device + tsFrom + tsTo + provider);
+        return provider.getAllActivitySamples(tsFrom, tsTo);
+    }
+
+
+}
+
