0159657bd5ac9426e6b5061b47f4b326f6602d15
Manuel Ruß <manuel_russ@dismail.de>
2019-10-28 22:40:07 +0100

    - AmazfitGTS Run basic functionalities (read data, ...)

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/amazfitgts/AmazfitGTSCoordinator.java
@@ -0,0 +1,71 @@
+package nodomain.freeyourgadget.gadgetbridge.devices.huami.amazfitgts;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nodomain.freeyourgadget.gadgetbridge.devices.InstallHandler;
+import nodomain.freeyourgadget.gadgetbridge.devices.huami.HuamiCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDeviceCandidate;
+import nodomain.freeyourgadget.gadgetbridge.model.DeviceType;
+
+public class AmazfitGTSCoordinator extends HuamiCoordinator {
+    private static final Logger LOG = LoggerFactory.getLogger(AmazfitGTSCoordinator.class);
+
+    @NonNull
+    @Override
+    public DeviceType getSupportedType(GBDeviceCandidate candidate) {
+        try {
+            BluetoothDevice device = candidate.getDevice();
+            String name = device.getName();
+            if (name != null && name.equalsIgnoreCase("Amazfit GTS")) {
+                return DeviceType.AMAZFITGTS;
+            }
+        } catch (Exception ex) {
+            LOG.error("unable to check device support", ex);
+        }
+        return DeviceType.UNKNOWN;
+    }
+
+    @Override
+    public DeviceType getDeviceType() {
+        return DeviceType.AMAZFITGTS;
+    }
+
+    @Override
+    public int getBondingStyle() {
+        return BONDING_STYLE_REQUIRE_KEY;
+    }
+
+    @Override
+    public InstallHandler findInstallHandler(Uri uri, Context context) {
+        return null;
+    }
+
+    @Override
+    public boolean supportsHeartRateMeasurement(GBDevice device) {
+        return true;
+    }
+
+    @Override
+    public boolean supportsWeather() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsActivityTracks() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsMusicInfo() {
+        return true;
+    }
+
+}
