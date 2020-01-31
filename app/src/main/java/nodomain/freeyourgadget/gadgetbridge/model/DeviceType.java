a943adbac7987d2ce5cb1f2cece9a3a0e3b41a04
TaaviE <taavi.eomae+github@gmail.com>
2020-01-31 19:38:13 +0200

    Added custom icon for iTag devices

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
@@ -64,7 +64,7 @@ public enum DeviceType {
     MAKIBESHR3(150, R.drawable.ic_device_default, R.drawable.ic_device_hplus_disabled, R.string.devicetype_makibes_hr3),
     BANGLEJS(160, R.drawable.ic_device_zetime, R.drawable.ic_device_zetime_disabled, R.string.devicetype_banglejs),
     MIJIA_LYWSD02(200, R.drawable.ic_device_pebble, R.drawable.ic_device_pebble_disabled, R.string.devicetype_mijia_lywsd02),
-    ITAG(250, R.drawable.ic_device_default, R.drawable.ic_device_default_disabled, R.string.devicetype_itag),
+    ITAG(250, R.drawable.ic_device_itag, R.drawable.ic_device_itag_disabled, R.string.devicetype_itag),
     VIBRATISSIMO(300, R.drawable.ic_device_lovetoy, R.drawable.ic_device_lovetoy_disabled, R.string.devicetype_vibratissimo),
     TEST(1000, R.drawable.ic_device_default, R.drawable.ic_device_default_disabled, R.string.devicetype_test);
 
