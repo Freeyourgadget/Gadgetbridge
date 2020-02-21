01ab7bcb54cad446bd149f7bb01c50ec30c7f97a
Andrzej Surowiec <emeryth@gmail.com>
2020-02-21 21:57:18 +0100

    Fix answering/rejecting calls on Android 9

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/receivers/GBCallControlReceiver.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/receivers/GBCallControlReceiver.java
@@ -16,9 +16,12 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.service.receivers;
 
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.telecom.TelecomManager;
 import android.telephony.TelephonyManager;
 
 import com.android.internal.telephony.ITelephony;
@@ -28,20 +31,26 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 
+import nodomain.freeyourgadget.gadgetbridge.GBApplication;
 import nodomain.freeyourgadget.gadgetbridge.deviceevents.GBDeviceEventCallControl;
 
 public class GBCallControlReceiver extends BroadcastReceiver {
     public static final String ACTION_CALLCONTROL = "nodomain.freeyourgadget.gadgetbridge.callcontrol";
     private static final Logger LOG = LoggerFactory.getLogger(GBCallControlReceiver.class);
+    private Context mContext = GBApplication.getContext();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         GBDeviceEventCallControl.Event callCmd = GBDeviceEventCallControl.Event.values()[intent.getIntExtra("event", 0)];
-        switch (callCmd) {
-            case END:
-            case REJECT:
-            case START:
-                try {
+
+        if (Build.VERSION.SDK_INT >= 28){
+            handleCallCmdTelecomManager(callCmd);
+        }else {
+            switch (callCmd) {
+                case END:
+                case REJECT:
+                case START:
+                    try {
                     TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
                     Class clazz = Class.forName(telephonyManager.getClass().getName());
                     Method method = clazz.getDeclaredMethod("getITelephony");
@@ -52,11 +61,32 @@ public class GBCallControlReceiver extends BroadcastReceiver {
                     } else {
                         telephonyService.answerRingingCall();
                     }
-                } catch (Exception e) {
-                    LOG.warn("could not start or hangup call");
-                }
-                break;
-            default:
+                    } catch (Exception e) {
+                        LOG.warn("could not start or hangup call");
+                    }
+                    break;
+                default:
+            }
+        }
+    }
+
+    @TargetApi(28)
+    public void handleCallCmdTelecomManager(GBDeviceEventCallControl.Event callCmd){
+        try {
+            TelecomManager tm = (TelecomManager) mContext.getSystemService(Context.TELECOM_SERVICE);
+
+            if (callCmd == GBDeviceEventCallControl.Event.END || callCmd == GBDeviceEventCallControl.Event.REJECT) {
+                tm.endCall();
+            }
+            else if  (callCmd == GBDeviceEventCallControl.Event.START || callCmd == GBDeviceEventCallControl.Event.ACCEPT) {
+                tm.acceptRingingCall();
+            }
+
+        }catch (SecurityException e){
+            LOG.warn("no permission to start or hangup call");
+        }
+        catch (Exception e) {
+            LOG.warn("could not start or hangup call");
         }
     }
 }
