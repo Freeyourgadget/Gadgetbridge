b40e33f0861543ef002ea935182bd84100c56dc4
Daniel Dakhno <dakhnod@gmail.com>
2020-11-12 12:11:54 +0100

    Fossil Hybrid: added "decline call" notification flag

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/requests/fossil/notification/PlayCallNotificationRequest.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/requests/fossil/notification/PlayCallNotificationRequest.java
@@ -9,7 +9,7 @@ public class PlayCallNotificationRequest extends PlayNotificationRequest {
     private final static int MESSAGE_ID_CALL = 1;
 
     public PlayCallNotificationRequest(String number, boolean callStart, FossilWatchAdapter adapter) {
-        super(callStart ? 1 : 7, callStart ? 8 : 2,
+        super(callStart ? 1 : 7, callStart ? 0b00011000 : 2,
                 ByteBuffer.wrap(new byte[]{(byte) 0x80, (byte) 0x00, (byte) 0x59, (byte) 0xB7}).order(ByteOrder.LITTLE_ENDIAN).getInt(),
                 number, "Incoming Call", MESSAGE_ID_CALL, adapter);
     }
