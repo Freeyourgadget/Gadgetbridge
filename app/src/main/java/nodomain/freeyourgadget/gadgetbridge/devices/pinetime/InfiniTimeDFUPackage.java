6403e13c9bc3a806e9c59f92c7d3e2ccd2e63449
TaaviE <taavi.eomae@gmail.com>
2021-05-29 15:26:05 +0300

    SerializedName should be a bit more explicit and theoretically allows reflectionless deserialization

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/pinetime/InfiniTimeDFUPackage.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/pinetime/InfiniTimeDFUPackage.java
@@ -16,34 +16,41 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.devices.pinetime;
 
-import androidx.annotation.Keep;
+import com.google.gson.annotations.SerializedName;
 
 import java.math.BigInteger;
 import java.util.List;
 
-@Keep
 public class InfiniTimeDFUPackage {
+    @SerializedName("manifest")
     InfiniTimeDFUPackageManifest manifest;
 }
 
-@Keep
 class InfiniTimeDFUPackageManifest {
+    @SerializedName("application")
     InfiniTimeDFUPackageApplication application;
+    @SerializedName("dfu_version")
     Float dfu_version;
 }
 
-@Keep
 class InfiniTimeDFUPackageApplication {
+    @SerializedName("bin_file")
     String bin_file;
+    @SerializedName("dat_file")
     String dat_file;
+    @SerializedName("init_packet_data")
     InfiniTimeDFUPackagePacketData init_packet_data;
 }
 
-@Keep
 class InfiniTimeDFUPackagePacketData {
+    @SerializedName("application_version")
     BigInteger application_version;
+    @SerializedName("device_revision")
     BigInteger device_revision;
+    @SerializedName("device_type")
     BigInteger device_type;
+    @SerializedName("firmware_crc16")
     BigInteger firmware_crc16;
+    @SerializedName("softdevice_req")
     List<Integer> softdevice_req;
 }
