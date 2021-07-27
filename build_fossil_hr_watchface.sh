5af73c3676ebe5ca1970232ffa6adade40dfd624
Andreas Shimokawa <shimokawa@fsfe.org>
2021-07-27 21:58:16 +0200

    add script to build fossil_hr_watchface by building submodules

---
+++ build_fossil_hr_watchface.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pushd jerryscript
+python3 tools/build.py --jerry-cmdline-snapshot ON
+popd
+pushd fossil-hr-watchface
+export jerry=../jerryscript/build/bin/jerry-snapshot
+$jerry generate -f '' open_source_watchface.js -o openSourceWatchface.bin
+$jerry generate -f '' widget_date.js -o widgetDate.bin
+$jerry generate -f '' widget_weather.js -o widgetWeather.bin
+$jerry generate -f '' widget_steps.js -o widgetSteps.bin
+$jerry generate -f '' widget_hr.js -o widgetHR.bin
+popd
+mv fossil-hr-watchface/*.bin app/src/main/assets/fossil_hr/
