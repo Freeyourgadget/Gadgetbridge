2d96328d1b1f3effe89ac86685db15425189a20d
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2021-09-14 15:04:12 +0200

    Fossil Hybrid HR: Lower battery level warning threshold to 10%

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/requests/fossil_hr/configuration/ConfigurationGetRequest.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/qhybrid/requests/fossil_hr/configuration/ConfigurationGetRequest.java
@@ -59,7 +59,7 @@ public class ConfigurationGetRequest extends FileEncryptedLookupAndGetRequest im
             }else if(item instanceof ConfigurationPutRequest.BatteryConfigItem){
                 device.setBatteryLevel((short) ((ConfigurationPutRequest.BatteryConfigItem) item).getBatteryPercentage());
                 device.setBatteryVoltage(((ConfigurationPutRequest.BatteryConfigItem) item).getBatteryVoltage() / 1000f);
-                device.setBatteryThresholdPercent((short) 15);
+                device.setBatteryThresholdPercent((short) 10);
 
                 GBDeviceEventBatteryInfo batteryInfo = new GBDeviceEventBatteryInfo();
                 batteryInfo.level = (short) ((ConfigurationPutRequest.BatteryConfigItem) item).getBatteryPercentage();
