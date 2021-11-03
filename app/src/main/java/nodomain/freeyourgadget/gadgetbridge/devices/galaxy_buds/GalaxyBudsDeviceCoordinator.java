a6a6bfef1710ccea5a14b91023121b9b0d3bef1e
vanous <petr@linuks.cz>
2021-11-03 16:06:54 +0100

    Galaxy Buds: add custom icons for batteries

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/galaxy_buds/GalaxyBudsDeviceCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/galaxy_buds/GalaxyBudsDeviceCoordinator.java
@@ -48,8 +48,8 @@ public class GalaxyBudsDeviceCoordinator extends AbstractDeviceCoordinator {
 
     @Override
     public BatteryConfig[] getBatteryConfig() {
-        BatteryConfig battery1 = new BatteryConfig(0, R.drawable.ic_tws_bud_l, R.string.left_earbud);
-        BatteryConfig battery2 = new BatteryConfig(1, R.drawable.ic_tws_bud_r, R.string.right_earbud);
+        BatteryConfig battery1 = new BatteryConfig(0, R.drawable.ic_galaxy_buds_l, R.string.left_earbud);
+        BatteryConfig battery2 = new BatteryConfig(1, R.drawable.ic_galaxy_buds_r, R.string.right_earbud);
         return new BatteryConfig[]{battery1, battery2};
     }
 
