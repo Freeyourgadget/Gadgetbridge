dbdeb59f9cdbf68effd1d40e3fe01611a9b4e396
Andreas Shimokawa <shimokawa@fsfe.org>
2021-12-12 19:01:54 +0100

    Mi Band 6: Whitelist FW 1.0.6.10

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/miband6/MiBand6FirmwareInfo.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/miband6/MiBand6FirmwareInfo.java
@@ -39,9 +39,11 @@ public class MiBand6FirmwareInfo extends HuamiFirmwareInfo {
         // firmware
         crcToVersion.put(47447, "1.0.1.36");
         crcToVersion.put(41380, "1.0.4.38");
+        crcToVersion.put(8209, "1.0.6.10");
         // resources
         crcToVersion.put(54803, "1.0.1.36");
         crcToVersion.put(14596, "1.0.4.38");
+        crcToVersion.put(63397, "1.0.6.10");
     }
 
     public MiBand6FirmwareInfo(byte[] bytes) {
