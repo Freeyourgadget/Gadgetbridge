37ba8c291e3b4f1ac15f118a435c862b76bef8f6
Andreas Shimokawa <shimokawa@fsfe.org>
2022-05-10 15:49:34 +0200

    Amazfit Band 5: Enbable lift wrist sensitivity and world clock configuration

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/amazfitband5/AmazfitBand5Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/amazfitband5/AmazfitBand5Coordinator.java
@@ -86,6 +86,16 @@ public class AmazfitBand5Coordinator extends HuamiCoordinator {
     }
 
     @Override
+    public int getWorldClocksSlotCount() {
+        return 20; // as enforced by Mi Fit
+    }
+
+    @Override
+    public int getWorldClocksLabelLength() {
+        return 30; // at least
+    }
+
+    @Override
     public int[] getSupportedDeviceSpecificSettings(GBDevice device) {
         return new int[]{
                 R.xml.devicesettings_amazfitband5,
@@ -94,7 +104,7 @@ public class AmazfitBand5Coordinator extends HuamiCoordinator {
                 R.xml.devicesettings_timeformat,
                 R.xml.devicesettings_dateformat,
                 R.xml.devicesettings_nightmode,
-                R.xml.devicesettings_liftwrist_display,
+                R.xml.devicesettings_liftwrist_display_sensitivity,
                 R.xml.devicesettings_swipeunlock,
                 R.xml.devicesettings_sync_calendar,
                 R.xml.devicesettings_reserve_reminders_calendar,
