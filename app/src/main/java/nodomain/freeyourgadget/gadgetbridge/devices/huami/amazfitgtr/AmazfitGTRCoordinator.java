785b6797a3c44833f10ff3889787cc9f292b10f5
José Rebelo <joserebelo@outlook.com>
2022-09-26 12:10:59 +0100

    Amazfit GTR: Enable button actions

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/amazfitgtr/AmazfitGTRCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/amazfitgtr/AmazfitGTRCoordinator.java
@@ -101,6 +101,7 @@ public class AmazfitGTRCoordinator extends HuamiCoordinator {
                 R.xml.devicesettings_reserve_reminders_calendar,
                 R.xml.devicesettings_expose_hr_thirdparty,
                 R.xml.devicesettings_bt_connected_advertisement,
+                R.xml.devicesettings_buttonactions_with_longpress,
                 R.xml.devicesettings_device_actions,
                 R.xml.devicesettings_high_mtu,
                 R.xml.devicesettings_overwrite_settings_on_connection,
