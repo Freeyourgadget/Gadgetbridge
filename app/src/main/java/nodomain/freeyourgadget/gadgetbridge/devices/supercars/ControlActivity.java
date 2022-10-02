d6383b5b6d4bddadda2d4270413be8167a5e75a5
vanous <petr@linuks.cz>
2022-10-02 16:46:13 +0200

    SuperCars: add blinking function, fine-tune driving

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/supercars/ControlActivity.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/supercars/ControlActivity.java
@@ -1,10 +1,14 @@
 package nodomain.freeyourgadget.gadgetbridge.devices.supercars;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
@@ -13,12 +17,14 @@ import org.slf4j.LoggerFactory;
 
 import nodomain.freeyourgadget.gadgetbridge.R;
 import nodomain.freeyourgadget.gadgetbridge.activities.AbstractGBActivity;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.supercars.SuperCarsSupport;
 
 public class ControlActivity extends AbstractGBActivity implements JoystickView.JoystickListener {
     private static final Logger LOG = LoggerFactory.getLogger(ControlActivity.class);
     LocalBroadcastManager localBroadcastManager;
     boolean lights = false;
+    boolean blinking = false;
     boolean turbo = false;
     CountDownTimer periodicDataSenderRunner;
 
@@ -26,14 +32,32 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
     SuperCarsConstants.Movement movement = SuperCarsConstants.Movement.IDLE;
     SuperCarsConstants.Speed speed = SuperCarsConstants.Speed.NORMAL;
     SuperCarsConstants.Light light = SuperCarsConstants.Light.OFF;
+    public GBDevice device;
+    TextView batteryPercentage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supercars_control);
-        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            device = bundle.getParcelable(GBDevice.EXTRA_DEVICE);
+        } else {
+            throw new IllegalArgumentException("Must provide a device when invoking this activity");
+        }
+
         CheckBox turboMode = findViewById(R.id.turboMode);
         CheckBox lightsOn = findViewById(R.id.lightsOn);
+        CheckBox lightsBlinking = findViewById(R.id.lightsBlinking);
+        batteryPercentage = findViewById(R.id.battery_percentage_label);
+        setBatteryLabel();
+        localBroadcastManager = LocalBroadcastManager.getInstance(ControlActivity.this);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(GBDevice.ACTION_DEVICE_CHANGED);
+        LocalBroadcastManager.getInstance(this).registerReceiver(commandReceiver, filter);
 
         turboMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -46,6 +70,24 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 lights = isChecked;
+                setLights();
+
+            }
+        });
+
+        lightsBlinking.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    if (lightsOn.isChecked()) {
+                        lightsOn.setChecked(false);
+                        lights = false;
+                    }
+                }
+                lightsOn.setEnabled(!isChecked);
+                blinking = isChecked;
+                setLights();
             }
         });
 
@@ -54,14 +96,39 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
         periodicDataSenderRunner.start();
     }
 
+    private void setLights() {
+        if (!blinking) {
+            if (lights) {
+                light = SuperCarsConstants.Light.ON;
+            } else {
+                light = SuperCarsConstants.Light.OFF;
+            }
+        }
+
+    }
+
+    private void setBatteryLabel() {
+        String level = device.getBatteryLevel() > 0 ? String.format("%1s%%", device.getBatteryLevel()) : device.getName();
+        //batteryPercentage.setText(level);  //the device seems to be cached, thus the value is old...!
+    }
+
     private void sendLocalBroadcast(Intent intent) {
         localBroadcastManager.sendBroadcast(intent);
     }
 
     public void periodicDataSender() {
-        periodicDataSenderRunner = new CountDownTimer(Long.MAX_VALUE, 200) {
+        periodicDataSenderRunner = new CountDownTimer(Long.MAX_VALUE, 100) {
 
             public void onTick(long millisUntilFinished) {
+
+                if (blinking) {
+                    if (light.equals(SuperCarsConstants.Light.ON)) {
+                        light = SuperCarsConstants.Light.OFF;
+                    } else {
+                        light = SuperCarsConstants.Light.ON;
+                    }
+                }
+
                 create_intent_with_data();
             }
 
@@ -83,9 +150,9 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
     @Override
     public void onJoystickMoved(float xPercent, float yPercent, int id) {
 
-        if (yPercent < 0) {
+        if (yPercent < 0.2 && yPercent != 0) {
             movement = SuperCarsConstants.Movement.UP;
-        } else if (yPercent > 0) {
+        } else if (yPercent > 0.2) {
             movement = SuperCarsConstants.Movement.DOWN;
         } else {
             movement = SuperCarsConstants.Movement.IDLE;
@@ -99,11 +166,6 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
             direction = SuperCarsConstants.Direction.CENTER;
         }
 
-        if (lights) {
-            light = SuperCarsConstants.Light.ON;
-        } else {
-            light = SuperCarsConstants.Light.OFF;
-        }
         if (turbo) {
             speed = SuperCarsConstants.Speed.TURBO;
         } else {
@@ -115,6 +177,7 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
     protected void onDestroy() {
         super.onDestroy();
         periodicDataSenderRunner.cancel();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(commandReceiver);
     }
 
     @Override
@@ -128,4 +191,15 @@ public class ControlActivity extends AbstractGBActivity implements JoystickView.
         super.onPause();
         periodicDataSenderRunner.cancel();
     }
+
+    BroadcastReceiver commandReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            LOG.debug("device receiver received " + intent.getAction());
+            if (intent.getAction().equals(GBDevice.ACTION_DEVICE_CHANGED)) {
+                setBatteryLabel();
+            }
+        }
+    };
+
 }
