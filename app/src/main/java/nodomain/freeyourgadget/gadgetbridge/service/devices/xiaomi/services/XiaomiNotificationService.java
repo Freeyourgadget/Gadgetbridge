f23347c47dc26c0a0dd684440a1fbdeceeca0e3a
José Rebelo <joserebelo@outlook.com>
2023-10-03 23:44:39 +0100

    Mi Band 8: Add broken notifications

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/xiaomi/services/XiaomiNotificationService.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/xiaomi/services/XiaomiNotificationService.java
@@ -19,16 +19,27 @@ package nodomain.freeyourgadget.gadgetbridge.service.devices.xiaomi.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import nodomain.freeyourgadget.gadgetbridge.BuildConfig;
 import nodomain.freeyourgadget.gadgetbridge.model.CallSpec;
 import nodomain.freeyourgadget.gadgetbridge.model.CannedMessagesSpec;
 import nodomain.freeyourgadget.gadgetbridge.model.NotificationSpec;
 import nodomain.freeyourgadget.gadgetbridge.proto.xiaomi.XiaomiProto;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.xiaomi.XiaomiSupport;
+import nodomain.freeyourgadget.gadgetbridge.util.StringUtils;
 
 public class XiaomiNotificationService extends AbstractXiaomiService {
     private static final Logger LOG = LoggerFactory.getLogger(XiaomiNotificationService.class);
 
+    private static final SimpleDateFormat TIMESTAMP_SDF = new SimpleDateFormat("yyyyMMdd'T'HHmmss", Locale.ROOT);
+
     public static final int COMMAND_TYPE = 7;
+
+    public static final int CMD_NOTIFICATION_SEND = 0;
 
     public XiaomiNotificationService(final XiaomiSupport support) {
         super(support);
@@ -40,7 +51,60 @@ public class XiaomiNotificationService extends AbstractXiaomiService {
     }
 
     public void onNotification(final NotificationSpec notificationSpec) {
-        // TODO
+        // TODO this is not working
+        if (true) {
+            LOG.warn("Notifications disabled, they're not working");
+            return;
+        }
+
+        final XiaomiProto.Notification3.Builder notification3 = XiaomiProto.Notification3.newBuilder()
+                .setId(notificationSpec.getId())
+                .setTimestamp(TIMESTAMP_SDF.format(new Date(notificationSpec.when)));
+
+        if (notificationSpec.sourceAppId != null) {
+            notification3.setPackage(notificationSpec.sourceAppId);
+        } else {
+            notification3.setPackage(BuildConfig.APPLICATION_ID);
+        }
+
+        final String senderOrTitle = StringUtils.getFirstOf(notificationSpec.sender, notificationSpec.title);
+        if (!senderOrTitle.isEmpty()) {
+            notification3.setTitle(senderOrTitle);
+        }
+
+        if (notificationSpec.body != null) {
+            notification3.setBody(notificationSpec.body);
+        }
+
+        if (notificationSpec.sourceName != null) {
+            notification3.setAppName(notificationSpec.sourceName);
+        }
+
+        // TODO what is this?
+        final String unknown12 = String.format(
+                Locale.ROOT,
+                "0|%s|%d|null|12345",
+                notification3.getPackage(),
+                notification3.getId() // i think this needs to be converted to unsigned
+        );
+        notification3.setUnknown12(unknown12);
+
+        final XiaomiProto.Notification2 notification2 = XiaomiProto.Notification2.newBuilder()
+                .setNotification3(notification3)
+                .build();
+
+        final XiaomiProto.Notification notification = XiaomiProto.Notification.newBuilder()
+                .setNotification2(notification2)
+                .build();
+
+        getSupport().sendCommand(
+                "send notification",
+                XiaomiProto.Command.newBuilder()
+                        .setType(COMMAND_TYPE)
+                        .setSubtype(CMD_NOTIFICATION_SEND)
+                        .setNotification(notification)
+                        .build()
+        );
     }
 
     public void onDeleteNotification(final int id) {
