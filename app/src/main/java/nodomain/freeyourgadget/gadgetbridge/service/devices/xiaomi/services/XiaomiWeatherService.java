929ea7ae57e19929610b3e7567280a130386da51
Andreas Shimokawa <shimokawa@fsfe.org>
2023-10-24 16:35:16 +0200

    Xiaomi: Implement sending current weather
    TODO: Support °F, find out about unknown values
    
    Tested on Mi Watch Lite

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/xiaomi/services/XiaomiWeatherService.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/xiaomi/services/XiaomiWeatherService.java
@@ -19,14 +19,14 @@ package nodomain.freeyourgadget.gadgetbridge.service.devices.xiaomi.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import nodomain.freeyourgadget.gadgetbridge.GBApplication;
-import nodomain.freeyourgadget.gadgetbridge.R;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import nodomain.freeyourgadget.gadgetbridge.activities.SettingsActivity;
-import nodomain.freeyourgadget.gadgetbridge.devices.huami.HuamiCoordinator;
-import nodomain.freeyourgadget.gadgetbridge.devices.miband.MiBandConst;
+import nodomain.freeyourgadget.gadgetbridge.devices.huami.HuamiWeatherConditions;
 import nodomain.freeyourgadget.gadgetbridge.model.WeatherSpec;
 import nodomain.freeyourgadget.gadgetbridge.proto.xiaomi.XiaomiProto;
-import nodomain.freeyourgadget.gadgetbridge.service.btle.TransactionBuilder;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.xiaomi.XiaomiSupport;
 import nodomain.freeyourgadget.gadgetbridge.util.Prefs;
 
@@ -37,6 +37,7 @@ public class XiaomiWeatherService extends AbstractXiaomiService {
 
     private static final int CMD_TEMPERATURE_UNIT_GET = 9;
     private static final int CMD_TEMPERATURE_UNIT_SET = 10;
+    private static final int CMD_SET_CURRENT_WEATHER = 0;
 
     public XiaomiWeatherService(final XiaomiSupport support) {
         super(support);
@@ -64,7 +65,57 @@ public class XiaomiWeatherService extends AbstractXiaomiService {
     }
 
     public void onSendWeather(final WeatherSpec weatherSpec) {
-        // TODO
+        String timestamp = new StringBuilder(
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)
+                        .format(new Date(weatherSpec.timestamp * 1000L)))
+                .insert(22, ':') // FIXME: I bet this fails for some, but all this java date craps sucks
+                .toString();
+
+
+        getSupport().sendCommand(
+                "set current weather",
+                XiaomiProto.Command.newBuilder()
+                        .setType(COMMAND_TYPE)
+                        .setSubtype(CMD_SET_CURRENT_WEATHER)
+                        .setWeather(XiaomiProto.Weather.newBuilder().setCurrent(
+                                XiaomiProto.WeatherCurrent.newBuilder()
+                                        .setTimeLocation(XiaomiProto.WeatherCurrentTimeLocation.newBuilder()
+                                                .setTimestamp(timestamp)
+                                                .setUnk2("")
+                                                .setCurrentLocationString(weatherSpec.location)
+                                        )
+                                        .setWeatherCondition(HuamiWeatherConditions.mapToAmazfitBipWeatherCode(weatherSpec.currentConditionCode)) // *SEEMS* to work
+                                        .setTemperature(XiaomiProto.WeatherCurrentTemperature.newBuilder()
+                                                .setDegrees(weatherSpec.currentTemp - 273) // TODO: support inches for weather
+                                                .setSymbol("℃")
+                                        )
+                                        .setHumidity(XiaomiProto.WeatherCurrentHumidity.newBuilder()
+                                                .setHumidity(weatherSpec.currentHumidity)
+                                                .setSymbol("%")
+                                        )
+                                        .setUnk5(XiaomiProto.WeatherCurrentUnk5.newBuilder()
+                                                .setUnk1("")
+                                                .setUnk2(0)
+                                        )
+                                        .setUnk6(XiaomiProto.WeatherCurrentUnk6.newBuilder()
+                                                .setUnk1("")
+                                                .setUnk2(0)
+                                        )
+                                        .setUnk7(XiaomiProto.WeatherCurrentUnk7.newBuilder()
+                                                .setUnk1("")
+                                                .setUnk2(0)
+                                        )
+                                        .setWarning(XiaomiProto.WeatherCurrentWarning.newBuilder()
+                                                .setCurrentWarning1(XiaomiProto.WeatherCurrentWarning1.newBuilder()
+                                                        .setCurrentWarningText("")
+                                                        .setCurrentWarningSeverityText("")
+                                                )
+                                        )
+                                        .setPressure(weatherSpec.pressure)
+
+                        ))
+                        .build()
+        );
     }
 
     private void setMeasurementSystem() {
