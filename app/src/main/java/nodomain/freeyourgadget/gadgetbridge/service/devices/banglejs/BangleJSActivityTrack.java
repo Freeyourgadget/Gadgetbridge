e7f8c9e7a6fa2c5bcac2ac9471600b203df719c7
Ganblejs <geatre+codeberg@gmail.com>
2024-03-06 12:13:50 +0100

    Bangle.js:actTrk:upd cmt re fetch all then parse

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/banglejs/BangleJSActivityTrack.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/banglejs/BangleJSActivityTrack.java
@@ -221,7 +221,7 @@ class BangleJSActivityTrack extends BangleJSDeviceSupport {
 
         if (!json.has("lines")) { // if no lines were sent with this json object, it signifies that the whole recorder log has been transmitted.
             setLatestFetchedRecorderLog(dir, log);
-            parseFetchedRecorderCSV(dir, filename, log, device, context); // TODO: Parsing could be done after we fetched all recorder logs instead, making the fetching quicker and maybe less prone to user errors.
+            parseFetchedRecorderCSV(dir, filename, log, device, context); // I tried refactoring to parse all fetched logs in one go at the end instead. But that only gave me more troubles. This seems like a more stable approach at least in the Bangle.js case.
             if (tracksList.length()==0) {
                 signalFetchingEnded(device, context);
                 int resetPacketCount = -1;
