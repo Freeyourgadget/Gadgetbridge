1a6288c74900f7bad63725ae6cda9a2560568e3a
Daniele Gobbetti <daniele+cb@gobbetti.name>
2024-03-24 16:32:51 +0100

    Garmin protocol: move field encode/decode interface to the FieldDefinition
    This allows for semantic subclassing the FieldDefinition.
    A FieldDefinitionTimestamp subclass is introduced as example

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/fit/fieldDefinitions/FieldDefinitionTimestamp.java
@@ -0,0 +1,26 @@
+package nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.fieldDefinitions;
+
+import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.FieldDefinition;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.baseTypes.BaseType;
+
+import static nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.GarminTimeUtils.GARMIN_TIME_EPOCH;
+
+public class FieldDefinitionTimestamp extends FieldDefinition {
+    public FieldDefinitionTimestamp(int localNumber, int size, BaseType baseType, String name) {
+        super(localNumber, size, baseType, name, 1, GARMIN_TIME_EPOCH);
+    }
+
+//    @Override
+//    public Object decode(ByteBuffer byteBuffer) {
+//        return new Timestamp((long) baseType.decode(byteBuffer, scale, offset) * 1000L);
+//    }
+//
+//    @Override
+//    public void encode(ByteBuffer byteBuffer, Object o) {
+//        if(o instanceof Timestamp) {
+//            baseType.encode(byteBuffer, (int) (((Timestamp) o).getTime() / 1000L), scale, offset);
+//            return;
+//        }
+//        baseType.encode(byteBuffer, o, scale, offset);
+//    }
+}
