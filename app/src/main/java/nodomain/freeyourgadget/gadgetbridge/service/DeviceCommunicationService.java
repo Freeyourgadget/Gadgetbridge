64a0b716f092f13fe9165a75d93799c0b5ca5385
José Rebelo <joserebelo@outlook.com>
2024-04-03 21:22:52 +0100

    Fix crash when connecting on some phones

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/DeviceCommunicationService.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/DeviceCommunicationService.java
@@ -52,6 +52,7 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -600,7 +601,7 @@ public class DeviceCommunicationService extends Service implements SharedPrefere
                     continue;
                 }
 
-                final Set<String> lastDeviceAddresses = prefs.getStringSet(GBPrefs.LAST_DEVICE_ADDRESSES, Collections.emptySet());
+                final Set<String> lastDeviceAddresses = new HashSet<>(prefs.getStringSet(GBPrefs.LAST_DEVICE_ADDRESSES, Collections.emptySet()));
 
                 if (!lastDeviceAddresses.contains(btDeviceAddress)) {
                     lastDeviceAddresses.add(btDeviceAddress);
