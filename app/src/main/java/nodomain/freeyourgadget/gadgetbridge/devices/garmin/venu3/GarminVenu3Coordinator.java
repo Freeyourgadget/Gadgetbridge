067dd8b2fb2cb3390dad4374d57c0e3942f4afe6
José Rebelo <joserebelo@outlook.com>
2024-04-23 23:18:52 +0100

    Garmin Venu 3: Enable canned replies

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/venu3/GarminVenu3Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/venu3/GarminVenu3Coordinator.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 import nodomain.freeyourgadget.gadgetbridge.R;
 import nodomain.freeyourgadget.gadgetbridge.devices.garmin.GarminCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
 
 public class GarminVenu3Coordinator extends GarminCoordinator {
     @Override
@@ -14,5 +15,10 @@ public class GarminVenu3Coordinator extends GarminCoordinator {
     @Override
     public int getDeviceNameResource() {
         return R.string.devicetype_garmin_vivomove_style;
+    }
+
+    @Override
+    public int getCannedRepliesSlotCount(final GBDevice device) {
+        return 16;
     }
 }
