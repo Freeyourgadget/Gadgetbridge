4152ec15706841a353168801f70165040816bb1b
kuhy <codeberg.org@kuhy.me>
2024-04-24 23:23:58 +0200

    Garmin protocol: add support for AGPS data retrieval

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/http/EphemerisHandler.java
@@ -0,0 +1,40 @@
+package nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.GarminSupport;
+
+public class EphemerisHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(EphemerisHandler.class);
+    private final GarminSupport deviceSupport;
+
+    public EphemerisHandler(GarminSupport deviceSupport) {
+        this.deviceSupport = deviceSupport;
+    }
+
+    public byte[] handleEphemerisRequest(final String path, final Map<String, String> query) {
+        try {
+            final File exportDirectory = deviceSupport.getWritableExportDirectory();
+            final File ephemerisDataFile = new File(exportDirectory, "CPE.BIN");
+            if (!ephemerisDataFile.exists() || !ephemerisDataFile.isFile()) {
+                throw new IOException("Cannot locate CPE.BIN file in export/import directory.");
+            }
+            final byte[] bytes = new byte[(int) ephemerisDataFile.length()];
+            final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(ephemerisDataFile));
+            final DataInputStream dis = new DataInputStream(bis);
+            dis.readFully(bytes);
+            return bytes;
+        } catch (IOException e) {
+            LOG.error("Unable to obtain ephemeris data.", e);
+            return null;
+        }
+    }
+}
