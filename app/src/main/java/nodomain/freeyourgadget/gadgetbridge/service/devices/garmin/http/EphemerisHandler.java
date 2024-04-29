9dfe3a52a0b036832b843a695c5fb54ebccbaf85
kuhy <codeberg.org@kuhy.me>
2024-04-29 16:21:27 +0200

    Garmin protocol: add AGPS data checks

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/http/EphemerisHandler.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/http/EphemerisHandler.java
@@ -8,13 +8,17 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 
 import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.GarminSupport;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.file.GarminAgpsDataType;
 import nodomain.freeyourgadget.gadgetbridge.util.FileUtils;
+import nodomain.freeyourgadget.gadgetbridge.util.GBTarFile;
 
 public class EphemerisHandler {
     private static final Logger LOG = LoggerFactory.getLogger(EphemerisHandler.class);
+    private static final String QUERY_CONSTELLATIONS = "constellations";
     private final GarminSupport deviceSupport;
 
     public EphemerisHandler(GarminSupport deviceSupport) {
@@ -23,6 +27,10 @@ public class EphemerisHandler {
 
     public byte[] handleEphemerisRequest(final String path, final Map<String, String> query) {
         try {
+            if (!query.containsKey(QUERY_CONSTELLATIONS)) {
+                LOG.debug("Query does not contain information about constellations; skipping request.");
+                return null;
+            }
             final File agpsFile = deviceSupport.getAgpsFile();
             if (!agpsFile.exists() || !agpsFile.isFile()) {
                 LOG.info("File with AGPS data does not exist.");
@@ -30,6 +38,20 @@ public class EphemerisHandler {
             }
             try(InputStream agpsIn = new FileInputStream(agpsFile)) {
                 final byte[] rawBytes = FileUtils.readAll(agpsIn, 1024 * 1024); // 1MB, they're usually ~60KB
+                final GBTarFile tarFile = new GBTarFile(rawBytes);
+                final String[] requestedConstellations = Objects.requireNonNull(query.get(QUERY_CONSTELLATIONS)).split(",");
+                for (final String constellation: requestedConstellations) {
+                    try {
+                        final GarminAgpsDataType garminAgpsDataType = GarminAgpsDataType.valueOf(constellation);
+                        if (!tarFile.containsFile(garminAgpsDataType.getFileName())) {
+                            LOG.error("AGPS archive is missing requested file: {}", garminAgpsDataType.getFileName());
+                            return null;
+                        }
+                    } catch (IllegalArgumentException e) {
+                        LOG.error("Device requested unsupported AGPS data type: {}", constellation);
+                        return null;
+                    }
+                }
                 LOG.info("Sending new AGPS data to the device.");
                 return rawBytes;
             }
