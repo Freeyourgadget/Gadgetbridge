b6468db13cde55e7f7e9d896ac9b38e5c008773b
Andreas Schneider <andreas_schn@web.de>
2024-04-30 21:23:02 +0200

    Garmin: add coordinator for Instinct Crossover

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/instinctcrossover/GarminInstinctCrossoverCoordinator.java
@@ -0,0 +1,19 @@
+package nodomain.freeyourgadget.gadgetbridge.devices.garmin.instinctcrossover;
+
+import nodomain.freeyourgadget.gadgetbridge.R;
+import nodomain.freeyourgadget.gadgetbridge.devices.garmin.GarminCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+
+import java.util.regex.Pattern;
+
+public class GarminInstinctCrossoverCoordinator extends GarminCoordinator {
+    @Override
+    protected Pattern getSupportedDeviceName() {
+        return Pattern.compile("Instinct Crossover");
+    }
+    
+    @Override
+    public int getDeviceNameResource() {
+        return R.string.devicetype_garmin_instinct_crossover;
+    }
+}
