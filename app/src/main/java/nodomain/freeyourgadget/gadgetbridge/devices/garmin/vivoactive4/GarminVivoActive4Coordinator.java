a0943649bd98d67729619ff1dae7515385595ac9
lorddavidiii <lorddavidiii@qi0.de>
2024-05-12 20:40:05 +0200

    Initial support for Garmin Vivoactive 4

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/vivoactive4/GarminVivoActive4Coordinator.java
@@ -0,0 +1,28 @@
+package nodomain.freeyourgadget.gadgetbridge.devices.garmin.vivoactive4;
+
+import java.util.regex.Pattern;
+
+import nodomain.freeyourgadget.gadgetbridge.R;
+import nodomain.freeyourgadget.gadgetbridge.devices.garmin.GarminCoordinator;
+
+public class GarminVivoActive4Coordinator extends GarminCoordinator {
+    @Override
+    protected Pattern getSupportedDeviceName() {
+        return Pattern.compile("vívoactive 4");
+    }
+
+    @Override
+    public int getDeviceNameResource() {
+        return R.string.devicetype_garmin_vivoactive_4;
+    }
+
+    @Override
+    public boolean supportsFlashing() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsAgpsUpdates() {
+        return true;
+    }
+}
